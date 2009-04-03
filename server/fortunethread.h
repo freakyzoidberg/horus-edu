4950a22611431f3f69b1a71effb62bcf3c16a9a7
Pierre <freakyzoidberg@users.noreply.github.com>
2009-04-03 13:02:42 +0000

    server multithread debug output classes a nettoyer

---
+++ server/fortunethread.h
@@ -0,0 +1,26 @@
+#ifndef FORTUNETHREAD_H
+#define FORTUNETHREAD_H
+
+#include <QThread>
+#include <QTcpSocket>
+
+//! [0]
+class FortuneThread : public QThread
+{
+    Q_OBJECT
+
+public:
+    FortuneThread(int socketDescriptor, const QString &fortune, QObject *parent);
+
+    void run();
+
+signals:
+    void error(QTcpSocket::SocketError socketError);
+
+private:
+    int socketDescriptor;
+    QString text;
+};
+//! [0]
+
+#endif
--- server/main.cpp
+++ server/main.cpp
@@ -1,10 +1,22 @@
+//#define QT_NO_DEBUG_OUTPUT
 #include <QtCore/QCoreApplication>
+#include <QtNetwork>
+#include <QtDebug>
+#include <stdlib.h>
+#include "fortuneserver.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    a.setApplicationName("test");
-    write(0, "coucou", 6);
-
+    FortuneServer server;
+    qDebug() << "Fortune Server Loaded";
+    if (!server.isListening())
+        qDebug() << "Server Dont Listen";
+    else
+    {
+        qDebug() << "Server Listen on port";
+        qDebug("port : %d", server.serverPort());
+    }
+    //connect();+
     return a.exec();
 }
