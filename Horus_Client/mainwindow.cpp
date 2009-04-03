aa69d436be6fb90bf8ee6142ab82b1ac59b20799
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-03 15:04:48 +0000

    [Client] set title to Horus

--- Horus_Client/mainwindow.cpp
+++ Horus_Client/mainwindow.cpp
@@ -5,6 +5,7 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindowClass)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Horus");
 }
 
 MainWindow::~MainWindow()
