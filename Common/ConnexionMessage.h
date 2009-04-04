c8147acfe3d0121fb0e2a787ef4b7c5f33f27c80
git <git@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-04 07:48:25 +0000

    add comments

--- Common/ConnexionMessage.h
+++ Common/ConnexionMessage.h
@@ -3,9 +3,19 @@
 
 #include "Defines.h";
 
+/*!
+ * First Message after connexion
+ * From Server to the Client
+ * After, the client answer with a "LoginContainer" class
+ */
 class ConnexionMessage
 {
+    /*!
+     * NEVER CHANGE THIS MESSAGE
+     * Just change the "serverVersion"
+     */
     const char servername   []             = "Horus Server";
+
     char       serverVersion[VERSION_SIZE];
 }
 
--- Common/LoginContainer.h
+++ Common/LoginContainer.h
@@ -1,14 +1,23 @@
 #ifndef LOGINCONTAINER_H
 #define LOGINCONTAINER_H
 
+#include "Defines.h"
+
 class CommunicationContainer
 {
  public:
+
+    char clientVersion[VERSION_SIZE];
+
     char login[LOGIN_SIZE];
-    enum authType{HASH_MD5};//Login with certificate or something else can be added
+
+    /*!
+     * Type of authenfication
+     * in the future we may want to add the ssl certificate for auth. or something else
+     */
+    enum authType{HASH_MD5};
+    uint  sizeCont;
     void* content;
 };
-
-#define AuthHashMD5 char test[32]
 
 #endif // LOGINCONTAINER_H
