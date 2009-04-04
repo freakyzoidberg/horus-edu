b9aa91e674dbc5a4b78006523a55ce02bcc24848
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-04 08:07:15 +0000

    [Client]  * add remember size and location of mainwindow

--- trunk/Horus_Client/main.cpp
+++ trunk/Horus_Client/main.cpp
@@ -5,6 +5,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QCoreApplication::setOrganizationName("Horus Inc.");
+    QCoreApplication::setOrganizationDomain("4--2.net");
+    QCoreApplication::setApplicationName("Horus Client");
+    QCoreApplication::setApplicationVersion("0.01");
     w.show();
     return a.exec();
 }
