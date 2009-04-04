0919ebc68c5aeb464344743c19b69e5c881230b5
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-04 08:07:18 +0000

    ClientAPI.h : add accessors

--- Horus_Client/ClientAPI.h
+++ Horus_Client/ClientAPI.h
@@ -7,12 +7,28 @@
 
 class IClientModule
 {
+  public:
+    virtual void setModName(const &std::string) = 0;
+    virtual void setModVersion(const &std::string) = 0;
+    virtual void setModConflicts(const &std::string, const &std::string) = 0;
+    virtual void setModRequired(const &std::string, const &std::string) = 0;
+    virtual void setModRecommended(const &std::string, const &std::string) = 0;
+    virtual void setExports(const void *) = 0;
+
+  public:
+    virtual const std::string                   getModName() = 0;
+    virtual const std::string                   getModVersion() = 0;
+    virtual const std::vector<std::string[2]>   getModConflicts() = 0;
+    virtual const std::vector<std::string[2]>   getModRequired() = 0;
+    virtual const std::vector<std::string[2]>   getModRecommended() = 0;
+    virtual const void                          *getExports() = 0;
+
   private:
-    virtual std::string                 modName = 0;
-    virtual std::string                 modVersion = 0;
-    virtual std::vector<std::string>    modConflicts = 0;
-    virtual std::vector<std::string>    modRequired = 0;
-    virtual std::vector<std::string>    modRecommended = 0;
+    virtual std::string                     modName = 0;
+    virtual std::string                     modVersion = 0;
+    virtual std::vector<std::string[2]>     modConflicts = 0;
+    virtual std::vector<std::string[2]>     modRequired = 0;
+    virtual std::vector<std::string[2]>     modRecommended = 0;
 
     virtual void    *exports = 0;
 
