0230aec82fb50ac24634860f76affd8cb58e8a29
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-04 07:11:42 +0000

    IModule.h: add events

--- trunk/Horus_Client/ClientAPI.h
+++ trunk/Horus_Client/ClientAPI.h
@@ -3,17 +3,21 @@
 
 #include <string>
 #include <vector>
+#include <QEvent>
 
 class IClientModule
 {
   private:
-    std::string                 modName;
-    std::string                 modVersion;
-    std::vector<std::string>    modConflicts;
-    std::vector<std::string>    modRequired;
-    std::vector<std::string>    modRecommended;
+    virtual std::string                 modName = 0;
+    virtual std::string                 modVersion = 0;
+    virtual std::vector<std::string>    modConflicts = 0;
+    virtual std::vector<std::string>    modRequired = 0;
+    virtual std::vector<std::string>    modRecommended = 0;
 
-    void    *exports;
+    virtual void    *exports = 0;
+
+    virtual QEvent::QEvent  eventDispather = 0;
+    virtual QEvent::QEvent  eventListener = 0;
 };
 
 #endif // CLIENTAPI_H
