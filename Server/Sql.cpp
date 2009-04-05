29e464d10fa5d4987753d93cce44f1feba0517a8
Pierre <freakyzoidberg@users.noreply.github.com>
2009-04-05 15:28:54 +0000

    sql server connection

--- Server/Sql.cpp
+++ Server/Sql.cpp
@@ -1,21 +1,26 @@
 #include "Sql.h"
-
+#define QT_DEBUG_COMPONENT
 sql::sql()
 {
 }
 
 bool sql::sqlconnect(QString dbName, QString hostname, QString username, QString password)
 {
-    qDebug() << "sql::sqlconnect() start";
+
+
     this->db= QSqlDatabase::addDatabase("QMYSQL");
-    qDebug() << "sql::sqlconnect() database added";
+
      this->db.setHostName(hostname);
      this->db.setDatabaseName(dbName);
      this->db.setUserName(username);
      this->db.setPassword(password);
-     qDebug() << "sql::sqlconnect() all param set";
-     //bool ok = db.open();
-     //qDebug() << db.lastError();
-     //qDebug() << "sql::sqlconnect() db opened";
-    return (0);
+     this->db.setPort(3306);
+     this->db.setConnectOptions("CLIENT_INTERACTIVE=1");
+     qDebug() << "Trying to connect to database " << this->db.databaseName() << " on " << this->db.userName() << "@" << this->db.hostName() << ":" << this->db.port();
+     if (!this->db.open())
+         qDebug() << "sql::sqlconnect() //hostname : " << hostname << " //dbName : " << dbName << " //username : " << username;
+         qDebug() << "sql::sqlconnect() DriverName = " << this->db.driver();
+         qDebug() << this->db.lastError();
+     bool ok = this->db.isOpen();
+     return (ok);
 }
