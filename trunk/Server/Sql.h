3912fce3b4c0dd88c47d638df9859aa6dbd2aea7
zoidberg <zoidberg@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-09 21:11:40 +0000

    [SERVER{ comments for doxygen on Sql.h

--- trunk/Server/Sql.h
+++ trunk/Server/Sql.h
@@ -1,19 +1,50 @@
 #ifndef SQL_H
 #define SQL_H
 
- #include <QtSql>
+#include <QtSql>
 #include <QMutex>
+//!  Sql Class
+/*!
+  SQL connection and query management for the server
+*/
 
 class Sql
 {
 public:
+    //! Sql constructor
+    /*!
+      lock the mutex
+    */
     Sql();
+    //! Sql constructor
+    /*!
+        unlock the mutex
+    */
     ~Sql();
     //private:
-
+     //! Query the database
+    /*!
+      \sa query()
+      \param thequer the query to be executed
+      \return QSqlQuery Object
+      */
     QSqlQuery* query(QString thequer);
+    //! Mutex for single access socket to database
     static QMutex mymute;
+
+    //! Connect to the database
+    /*!
+      \sa sqlconnect()
+      \param dbname the database name
+      \param hostname the database hostname
+      \param username database username
+      \param password database password
+      \param driver the sql driver used for the connection (eg QMYSQL QPGSQL etc)
+      \param port database port
+    */
     bool sqlconnect(QString dbName, QString hostname, QString username, QString password, QString driver, QString port);
+
+
 };
 
 #endif
