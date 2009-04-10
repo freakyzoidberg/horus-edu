bfc2bd3f30c1e301f9dc36bc980c3d9dda2bb138
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-10 09:03:00 +0000

    [Client]  * Reverse stream of events (Now user events come from father to son)  * Delete bad initialisation of NetworkManager

--- Client/PluginManager.h
+++ Client/PluginManager.h
@@ -2,11 +2,13 @@
 #define PLUGINMANAGER_H
 
 #include <QObject>
+#include <QEvent>
 
 class PluginManager : public QObject
 {
 public:
     PluginManager(QObject *parent = 0);
+    bool event(QEvent *event);
 
 private:
     void    loadPlugins();
