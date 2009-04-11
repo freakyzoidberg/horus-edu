b7f2d4503289b557ed480e5ec62233488280b62f
Pierre <freakyzoidberg@users.noreply.github.com>
2009-04-11 18:01:42 +0000

    [SERVER] Multiple SQL connection managed by semaphores

--- Server/Sql.cpp
+++ Server/Sql.cpp
@@ -1,46 +1,75 @@
 #include "Sql.h"
 
+
 QMutex Sql::mymute;
+QSemaphore Sql::mtsema(SQLCONNECTIONCOUNT);
+bool Sql::con[SQLCONNECTIONCOUNT];
+
 Sql::Sql()
 {
     mymute.lock();
+    mtsema.acquire(1);
+    for (int i = 0; i < SQLCONNECTIONCOUNT; i++)
+    {
+       if (con[i] == true)
+        {
+            con[i] = false;
+            currentcon = i;
+            break;
+       }
+   }
+    mymute.unlock();
 }
 
 Sql::~Sql()
 {
-    mymute.unlock();
+    if ((currentcon >= 0) && (currentcon < SQLCONNECTIONCOUNT))
+    con[currentcon] = true;
+    mtsema.release(1);
+    //mymute.unlock();
 }
 bool Sql::sqlconnect(QString dbName, QString hostname, QString username, QString password, QString driver, QString port)
 {
-QSqlDatabase db;
 
-    db= QSqlDatabase::addDatabase(driver);
+bool ok = false;
+for (int i = 0; i < SQLCONNECTIONCOUNT; i++)
+{
+    QString connm = "con";
+    connm.append(QVariant(i).toString());
+    QSqlDatabase db = QSqlDatabase::addDatabase(driver, connm);
 
      db.setHostName(hostname);
      db.setDatabaseName(dbName);
      db.setUserName(username);
      db.setPassword(password);
      db.setPort(port.toInt());
+
     // db.setConnectOptions("CLIENT_INTERACTIVE=1");
-     qDebug() << "sql::sqlconnect() Trying to connect to database " << db.databaseName() << " on " << db.userName() << "@" << db.hostName() << ":" << db.port() << " using driver = " << db.driver();
+     qDebug() << "sql::sqlconnect() connect to" << db.databaseName() << " on " << db.userName() << "@" << db.hostName() << ":" << db.port() << "driver = " << db.driver();
      if (!db.open())
      {
          qDebug() << "sql::sqlconnect() //hostname : " << hostname << " //dbName : " << dbName << " //username : " << username;
          qDebug() << "sql::sqlconnect() DriverName = " << db.driver();
          qDebug() << db.lastError();
+         con[i] = false;
+         break;
      }
-     bool ok = db.isOpen();
+     else
+        con[i] = true;
+     ok = db.isOpen();
+ }
      return (ok);
 }
 
+
 QSqlQuery* Sql::query(QString thequer)
 {
+    QString connm = "con";
+    connm.append(QVariant(currentcon).toString());
+    QSqlDatabase localdb = QSqlDatabase::database(connm);
 
-    //QSqlDatabase localdb = QSqlDatabase::database();
-    //qDebug() << "used DB hostname is " << localdb.hostName();
-
-    QSqlQuery* query = new QSqlQuery(thequer);
-
+    QSqlQuery* query = new QSqlQuery(thequer, localdb);
+    //qDebug() << "used DB = " << connm;
 
 return (query);
 }
--- Server/Sql.h
+++ Server/Sql.h
@@ -1,6 +1,6 @@
 #ifndef SQL_H
 #define SQL_H
-
+#include "../Common/Defines.h"
 #include <QtSql>
 #include <QMutex>
 //!  Sql Class
@@ -17,7 +17,6 @@ public:
     //! Sql destructor
     /*! unlock the mutex */
     ~Sql();
-
     //! Query the database
     /*!
       \sa query()
@@ -25,9 +24,11 @@ public:
       \return QSqlQuery Object
       */
     QSqlQuery* query(QString thequer);
-    //! Mutex for single access socket to database
+    //! Mutex for single access to Array of available connection
     static QMutex mymute;
-    //! Connect to the database
+    //! Semaphore for single access socket to database
+    static QSemaphore mtsema;
+    //! Connections to the database
     /*!
       \sa sqlconnect()
       \param dbName the database name
@@ -38,13 +39,21 @@ public:
       \param port database port
     */
     bool sqlconnect(QString dbName, QString hostname, QString username, QString password, QString driver, QString port);
-
+private:
+    //! Array of status of SQL connections
+    static bool con[SQLCONNECTIONCOUNT];
+    //! Connection used for the current instance of object
+    int currentcon;
 
 };
 
 #endif
 
 /*
+
+
+
+
 
         Sql *Mycon = new Sql();
         QSqlQuery* query = Mycon->query("SELECT * FROM testdb");
