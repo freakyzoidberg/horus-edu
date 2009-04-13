7b7c57704d36310f61391d062612318ae0fd7cfa
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-13 22:11:33 +0000

    [Client] * Clean mai.cpp * better debug on PluginManager

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,12 +1,7 @@
-#include <QtPlugin>
-#include <QObject>
 #include "ClientApplication.h"
-
-//Q_IMPORT_PLUGIN(testPlugin)
 
 int main(int argc, char *argv[])
 {
-
     ClientApplication a(argc, argv);
     return (a.exec());
 }
