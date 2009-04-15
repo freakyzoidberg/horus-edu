24328457589d42887f3396ce949409721d142696
zoidberg <zoidberg@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-04-15 20:27:13 +0000

    [Wizard] * HorusWizard UI * HorusWizard dumb sample

---
+++ trunk/HorusWizard/HorusWizard.pro
@@ -0,0 +1,10 @@
+# -------------------------------------------------
+# Project created by QtCreator 2009-04-15T14:29:38
+# -------------------------------------------------
+TARGET = HorusWizard
+TEMPLATE = app
+SOURCES += main.cpp \
+    mainwindow.cpp
+HEADERS += mainwindow.h
+FORMS += mainwindow.ui
+RESOURCES += Horusrc.qrc
---
+++ trunk/HorusWizard/mainwindow.cpp
@@ -0,0 +1,52 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindowClass)
+{
+
+
+
+    ui->setupUi(this);
+    ui->progressBar->reset();
+    ui->progressBar->setMinimum(0);
+
+    ui->progressBar_2->reset();
+
+    connect(ui->pushButton, SIGNAL( clicked() ), this, SLOT( InstallServer() ) );
+    connect(ui->pushButton_2, SIGNAL( clicked() ), this, SLOT( InstallClient() ) );
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::InstallServer()
+{
+ui->progressBar->setMaximum( 100000);
+for (int i = 0; i < 100001; i++) {
+     ui->progressBar->setValue((int)((i)));
+
+}
+ui->label_3->setText("Server Done !!!");
+}
+
+void MainWindow::InstallClient()
+{
+ui->progressBar->setMaximum( 100000);
+for (int i = 0; i < 100001; i++) {
+     ui->progressBar_2->setValue((int)((i)));
+}
+
+ui->label_4->setText("Client Done !!!");
+}
+
+void MainWindow::done()
+{
+    //timer.start( 1 );
+
+    //lineEdit->setText( path );
+}
