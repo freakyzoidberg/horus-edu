33151eb09caf965f41d56d7fa67163ab9ab52b7d
zoidberg <zoidberg@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-07 05:33:50 +0000

    [[Server]]
    Tree right management to implement

---
+++ trunk/Server/TreeMngt.cpp
@@ -0,0 +1,5 @@
+#include "TreeMngt.h"
+
+TreeMngt::TreeMngt()
+{
+}
---
+++ trunk/Server/TreeMngt.h
@@ -0,0 +1,10 @@
+#ifndef TREEMNGT_H
+#define TREEMNGT_H
+
+class TreeMngt
+{
+public:
+    TreeMngt();
+};
+
+#endif // TREEMNGT_H
