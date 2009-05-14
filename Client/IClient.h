10ffed28374b0dc189ad256f0a59f7b1e9c5f15c
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-14 23:11:07 +0000

    [[Client]] * Doxygen for the interface IClient

--- Client/IClient.h
+++ Client/IClient.h
@@ -1,15 +1,36 @@
 #ifndef ICLIENT_H
 #define ICLIENT_H
 
-#include "../Common/ModulePacket.h"
+#include "../Common/PluginPacket.h"
 #include "IClientPlugin.h"
 
 //! Interface given to each client plugin to communicate with the client
+/*!
+    This interface implements network relative functions which must be in a specific network interface
+*/
 class IClient
 {
 public:
+    //! A function for providing to plugins the send of packets to the network
+    /*!
+        need to be in a network interface
+        \param packet the packet to send
+     */
     virtual void            sendPacket(const ModulePacket& packet) const = 0;
+    //! A function for providing to plugins the read of packets to the network
+    /*!
+        need to be in a network interface
+        \param packet a reference to the emplacement of the readed packet
+     */
     virtual void            recvPacket(const ModulePacket& packet) const = 0;
+    //! A function for accessing plugins from other plugins
+    /*!
+        This function return the instance of plugin from his name.
+        A plugin can only be instancieted once, so each name is unique.
+        It is destinated for plugins willing to contacts other plugins
+        \param name the name of the desired plugin
+        \return a pointer to the instance of the desired plugin or NULL if not found
+    */
     virtual IClientPlugin   *getPlugin(const QByteArray name) const = 0;
 };
 
