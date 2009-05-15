29ca0746e33de4f5212cf64f68936fa2f7543eee
zoidberg <zoidberg@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-15 16:06:47 +0000

    [[Server]] refs 57
    * finished update vector method

--- trunk/Server/Tree.cpp
+++ trunk/Server/Tree.cpp
@@ -10,36 +10,46 @@ Tree::~Tree()
 
 Tree* Tree::GetNodebyId(int id)
 {
-    int tmpid;
-    tmpid = id;
+    QMap<int, Tree::Tree*>::const_iterator it = maptree.find(id);
+    if (it != maptree.end())
+        return it.value();
+    else
+    {
+        return 0;
+    }
 }
 
 bool Tree::UpdateVector()
 {
+
     qDebug() << "updating vector from database";
     Sql con;
-    Tree *tempnode = new Tree();
+
     QSqlQuery query1("SELECT * FROM treemanagement", QSqlDatabase::database(con));
 
     maptree.clear();
+
     while (query1.next())
     {
+       Tree *tempnode = new Tree();
        tempnode->id = query1.value(0).toInt();
-
        tempnode->user_ref = query1.value(2).toInt();
        tempnode->parent_id = query1.value(3).toInt();
        maptree.insert(tempnode->id, tempnode);
    }
-/*
- for(QMap<int, TreeMngt::node>::const_iterator it = maptree.begin(); it != maptree.end(); ++it)
+
+ for(QMap<int, Tree::Tree*>::const_iterator it = maptree.begin(); it != maptree.end(); ++it)
   {
-     QMap<int, TreeMngt::node>::iterator it2 = maptree.find((*it).parent_id);
+     QMap<int, Tree::Tree*>::iterator it2 = maptree.find((*it)->parent_id);
      if (it2 != maptree.end())
      {
-         it2.value().sons.insert(it.value().id, it.value().id);
+         it2.value()->sons.insert(it.value()->id, it.value());
+         it.value()->parent = it2.value();
+         qDebug() << "link" << it.value()->id << " to " <<  it2.value()->id;
      }
   }
-  */
+
+
  //vecshow(vectree);
  return true;
 }
