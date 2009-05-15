4e0141b912e546371ad6aa7fadd4484dd8970ea1
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-15 08:46:40 +0000

    [[Doc]] * Add doxygen of MainWindow

--- Client/MainWindow.h
+++ Client/MainWindow.h
@@ -4,27 +4,43 @@
 #include <QMainWindow>
 #include "ui_MainWindow.h"
 
+//! Graphical class of the MainWindow
+/*!
+ *  Class containing the menus and the others widgets displayed.
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    //! Constructor
+    /*!
+        Initialize UI, create actions and menus
+        \param parent The parent
+     */
     MainWindow(QObject *parent);
 
 private slots:
+    //! Display a dialog for editing the settings
     void editSettings();
 
 private:
+    //! Create actions used in menus
     void createActions();
+    //! Create menus of the window
     void createMenus();
 
+    //! The graphical ui used for the MainWindow
     Ui::MainWindow ui;
+    //! A pointer to its parent
     QObject *parent;
-
+    //! The menu regrouping the mains actions of the Client
     QMenu   *fileMenu;
+    //! The menu regrouping parameters and settings
     QMenu   *editMenu;
-
+    // The action which exit the application
     QAction *exitAction;
+    // The action which edit the settings
     QAction *settingsAction;
 };
 
