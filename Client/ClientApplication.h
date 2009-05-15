3c24be6fea07cc131fe235cfe5707e2dd59f3859
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-15 07:26:09 +0000

    [[Doc]] * Add doxygen of IClientPlugin, IClient, IPluginNetwork

--- Client/ClientApplication.h
+++ Client/ClientApplication.h
@@ -5,35 +5,35 @@
 
 //! Main class representing the application
 /*!
-    This class initalize the software, create the Loader and the MainWindow.
-    Provide some functions for reset plugins or network.
-*/
+ *  This class initalize the software, create the Loader and the MainWindow.
+ *  Provide some functions for reset plugins or network.
+ */
 class ClientApplication : public QApplication
 {
 public:
     //! Constructor
     /*!
-        Initialize names and version of the application.
-        Instanciate NetworkManager and PluginManager
-        Create MainWindow but don't show it for now
-        Give the hand to the Loader
-    */
+     *  Initialize names and version of the application.
+     *  Instanciate NetworkManager and PluginManager
+     *  Create MainWindow but don't show it for now
+     *  Give the hand to the Loader
+     */
     ClientApplication(int argc, char *argv[]);
     //! Destructor
     /*!
-        Terminate each composant of the application.
-        Because of threads we can't just call exit !
-        \param argc the argc of the main function
-        \param argv the argv of the main function
-    */
+     *  Terminate each composant of the application.
+     *  Because of threads we can't just call exit !
+     *  \param argc the argc of the main function
+     *  \param argv the argv of the main function
+     */
     ~ClientApplication();
     //! Surcharge of the event function
     /*!
-        Useless for now.
-        May be deleted in the future.
-        \param event the event received
-        \return the accept status of the event
-    */
+     *  Useless for now.
+     *  May be deleted in the future.
+     *  \param event the event received
+     *  \return the accept status of the event
+     */
     bool    event(QEvent *event);
     //! Unload then load again all the plugins
     void    reloadPlugins();
--- Client/IPluginNetwork.h
+++ Client/IPluginNetwork.h
@@ -3,11 +3,18 @@
 
 #include <QEvent>
 
+//! Interface optionnal for using the network
+/*!
+ *  Each plugin implementing this interface will be able to use the network: send and receive packets.
+ */
 class IPluginNetwork
 {
     public:
+        //! Useless, must be deleted
         virtual bool    eventHandler(QEvent *) = 0;
+        //! Must be moved in a interface of the client like IClient
         virtual void    retrievedPacket() = 0;
+        //! Must be moved in a interface of the client like IClient
         virtual void    buildPacket() = 0;
 };
 
