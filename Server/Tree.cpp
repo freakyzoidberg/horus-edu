01c56aacd63c5c6a08d7102a3a4f5fb40dad6dff
Pierre <freakyzoidberg@users.noreply.github.com>
2009-05-15 15:15:30 +0000

    [[Server]] refs 57
    * Declaration of private members
    * start of map generation method

--- Server/Tree.cpp
+++ Server/Tree.cpp
@@ -1,9 +1,45 @@
 #include "Tree.h"
-
+QMap<int, Tree*> Tree::maptree;
 Tree::Tree()
 {
 }
 
 Tree::~Tree()
 {
+}
+
+Tree* Tree::GetNodebyId(int id)
+{
+    int tmpid;
+    tmpid = id;
+}
+
+bool Tree::UpdateVector()
+{
+    qDebug() << "updating vector from database";
+    Sql con;
+    Tree *tempnode = new Tree();
+    QSqlQuery query1("SELECT * FROM treemanagement", QSqlDatabase::database(con));
+
+    maptree.clear();
+    while (query1.next())
+    {
+       tempnode->id = query1.value(0).toInt();
+
+       tempnode->user_ref = query1.value(2).toInt();
+       tempnode->parent_id = query1.value(3).toInt();
+       maptree.insert(tempnode->id, tempnode);
+   }
+/*
+ for(QMap<int, TreeMngt::node>::const_iterator it = maptree.begin(); it != maptree.end(); ++it)
+  {
+     QMap<int, TreeMngt::node>::iterator it2 = maptree.find((*it).parent_id);
+     if (it2 != maptree.end())
+     {
+         it2.value().sons.insert(it.value().id, it.value().id);
+     }
+  }
+  */
+ //vecshow(vectree);
+ return true;
 }
