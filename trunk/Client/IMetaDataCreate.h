0d1348cf402ba9c4a2ee0c92c6898e1fd2d9441c
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-17 22:54:00 +0000

    [client] * add begining of PDF API (will be modified soon)

---
+++ trunk/Client/IMetaDataCreate.h
@@ -0,0 +1,14 @@
+#ifndef IMETADATACREATE_H
+#define IMETADATACREATE_H
+
+//! this class inheritaded from IMetadata. It is used to create a metadata file.
+/*!
+  This class contains method to generate the metadata
+  used to customize the PDF file.
+*/
+class   IMetaDataCreate : public IMetadata
+{
+    public:
+};
+
+#endif // IMETADATACREATE_H
---
+++ trunk/Client/IPdfRendering.h
@@ -0,0 +1,68 @@
+#ifndef IPDFRENDERING_H
+#define IPDFRENDERING_H
+
+#include <qt4/poppler-qt4.h>
+
+//! Class of the API used to render PDF files
+/*!
+    This class contains methods allowing to render a PDF virtual
+    All of them are pure virtual. Modules using PDF files must
+    implement this class
+
+    Maybe some methods can be delete, im not sure right now
+*/
+class   IPdfRendering
+{
+    public:
+
+    //! return the name of the PDF file
+    virtual const QString & getFileName() const = 0;
+
+    //! return the poppler document
+    /*!
+      \return the variable pdfDoc;
+    */
+    virtual const Poppler::Document    *getPdfDoc() const = 0;
+
+    //! return the current page
+    virtual const Poppler::Page         *getPage() const = 0;
+
+    //! return the number of the current page
+    virtual int   getCurrentPageNumber() const = 0;
+
+    //! open the pdfFile
+    virtual void        openFile() const = 0;
+
+    //! close the pdfFile
+    virtual void        closeFile() const = 0;
+
+    //! reload the file
+    virtual void        reloadFile() const = 0;
+
+    //! set one from the pdf document, check if the page is not out of range
+    /*!
+      \param pageNb the number of the page you wanna load
+    */
+    virtual void        loadPage(int pageNb) = 0;
+
+    //! return the scaleFactor
+    virtual float       getScaleFactor() const = 0;
+
+    //! create the picture of the PDF, not finished. (need a solution to
+    //! pass this image to the "core")
+    virtual void    render() = 0;
+
+    //! modification of the scale factor, used when user zoom
+    /*!
+      \param scaleFactor the new scaleFactor
+    */
+    virtual void    newScaleFactor(float scaleFactor) = 0;
+
+    //! display an other page
+    /*!
+      \param pageNumber the new page the user want to display
+    */
+    virtual void    newPage(int pageNumber) = 0;
+};
+
+#endif // IPDFRENDERING_H
