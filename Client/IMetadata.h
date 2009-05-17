9cf8d99d9dbaf624b683b3c410b758eea3ee2f47
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-17 22:54:00 +0000

    [client] * add begining of PDF API (will be modified soon)

---
+++ Client/IMetadata.h
@@ -0,0 +1,14 @@
+#ifndef IMETADATA_H
+#define IMETADATA_H
+
+//! Access to the medata of a PDF files
+/*!
+    This class contains method allowing to read the medata
+    used to customize the PDF file.
+*/
+class   IMetaData
+{
+    public:
+};
+
+#endif // IMETADATA_H
