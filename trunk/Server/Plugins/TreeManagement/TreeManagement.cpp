30be25afb8169680bbf978edda0d36ac828d293f
zoidberg <zoidberg@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-27 18:52:25 +0000

    [[Server]] [[TreeManagement Plugin]] refs #77
    create some method to get tree and set node for admin

--- trunk/Server/Plugins/TreeManagement/TreeManagement.cpp
+++ trunk/Server/Plugins/TreeManagement/TreeManagement.cpp
@@ -4,6 +4,8 @@ Q_EXPORT_PLUGIN2(TreeManagement, TreeManagement);
 
 TreeManagement::TreeManagement()
 {
+    requestFunctions["getTree"] = &TreeManagement::gettree;
+    requestFunctions["setNode"] = &TreeManagement::setnode;
     qDebug() << "TreeManagement has arrived";
 }
 
@@ -14,27 +16,39 @@ TreeManagement::~TreeManagement()
 
 void TreeManagement::recvPacket(quint32 userId, const PluginPacket& packet)
 {
-    server->sendPacket(userId, PluginPacket(packet.sourcePlugin, packet.data));
-    QByteArray* toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
-    toto = server->GetSql();
-    delete toto;
+    const QVariantHash request = packet.data.toHash();
+    QVariantHash response;
+
+    response["Request"] = request["Request"];
+
+    //fill response
+    (this->*requestFunctions.value(request["Request"].toByteArray(),//go to the target method
+                                   &TreeManagement::unknownRequest)) //or if it'sunknown request
+                                                                    (request, response);//parameters
+
+    //send response
+    server->sendPacket(userId, PluginPacket(packet.sourcePlugin, response));
+}
+
+void  TreeManagement::unknownRequest(const QVariantHash& request,QVariantHash& response)
+{
+
+}
+
+void  TreeManagement::gettree(const QVariantHash& request,QVariantHash& response)
+{
+
+}
+
+void  TreeManagement::setnode(const QVariantHash& request,QVariantHash& response)
+{
+}
+
+int TreeManagement::getidofusernode(const QVariantHash &request)
+{
+    QSqlQuery query1 = server->getSqlQuery();
+    query1.prepare("SELECT id_group FROM user_has_group WHERE id_user =?");
+    query1.addBindValue("1");
+    query1.exec();
+    return 1;
 }
