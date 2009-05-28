0c86687702f596945825445fefe6a323272ebf06
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-28 07:18:56 +0000

    [[Client]] refs #68 * Add specific rows in SettingsDialog if not already defined in the config file

--- Client/SettingsDialog.cpp
+++ Client/SettingsDialog.cpp
@@ -33,13 +33,34 @@ void SettingsDialog::FillPluginTab()
 
     pluginLayout = new QFormLayout(this->ui.PluginTab);
     settings->beginGroup("Plugins");
-    qDebug() << settings->childKeys();
     foreach (QString key, settings->childKeys())
         pluginLayout->addRow(key, new QLineEdit(settings->value(key).toString()));
+    if (!settings->childKeys().contains("Load", Qt::CaseInsensitive))
+        pluginLayout->insertRow(0, "Load", new QLineEdit(""));
+    if (!settings->childKeys().contains("SystemDirectoryPath", Qt::CaseInsensitive))
+        pluginLayout->insertRow(0, "SystemDirectoryPath", new QLineEdit(""));
+    if (!settings->childKeys().contains("UserDirectoryPath", Qt::CaseInsensitive))
+        pluginLayout->insertRow(0, "UserDirectoryPath", new QLineEdit(""));
     settings->endGroup();
     ui.PluginTab->setLayout(pluginLayout);
 }
 
 void SettingsDialog::FillNetworkTab()
 {
+    QFormLayout *networkLayout;
+
+    networkLayout = new QFormLayout(this->ui.NetworkTab);
+    settings->beginGroup("Network");
+    foreach (QString key, settings->childKeys())
+        networkLayout->addRow(key, new QLineEdit(settings->value(key).toString()));
+    if (!settings->childKeys().contains("Server", Qt::CaseInsensitive))
+        networkLayout->insertRow(0, "Server", new QLineEdit(""));
+    if (!settings->childKeys().contains("Port", Qt::CaseInsensitive))
+        networkLayout->insertRow(0, "Port", new QLineEdit(""));
+    settings->endGroup();
+    settings->beginGroup("SESSIONS");
+    foreach (QString key, settings->childKeys())
+        networkLayout->addRow(key, new QLineEdit(settings->value(key).toString()));
+    settings->endGroup();
+    ui.NetworkTab->setLayout(networkLayout);
 }
