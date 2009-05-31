cb5e6c1b6029302e704dabe3a84e2ef8c2b0c4ac
abderman <abderman@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-05-31 20:30:55 +0000

    [[Client]] fix #81 * link the login window with the loader window

--- trunk/Client/LoginDialog.h
+++ trunk/Client/LoginDialog.h
@@ -5,26 +5,30 @@
 #include "ui_LoginDialog.h"
 #include "ClientEvents.h"
 #include "../Common/CommLogin.h"
-
+#include "ClientApplication.h"
 
 //! This Object is the window to login in the server with a username and password
 class LoginDialog : public QWidget {
     Q_OBJECT
 public:
-    LoginDialog(QObject *parent = 0);
+    LoginDialog(ClientApplication *parent);
 
     ~LoginDialog();
 
 private:
     //! The graphical ui used
     Ui::Form l_ui;
-
-    //! LoginDialog's parent to send event to NetworkManager
-    QObject *parent;
+    //! Overload of the event method
+    /*!
+     *  \param event the event received
+     *  \return the accept status of the event
+     */
+    void closeEvent(QCloseEvent *event);
+    //! A pointer to its parent ClientApplication
+    ClientApplication *parent;
 
 private slots:
     //! callback of the connect button, it send an event to NetworkManager
-    void on_Form_destroyed();
     void on_connectButton_clicked();
 };
 
