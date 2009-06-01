b10d17a68db443fa522fcaa983dc69fc470e77e9
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-06-01 16:04:19 +0000

    Client * make it compile

--- Client/Network.h
+++ Client/Network.h
@@ -1,4 +1,20 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include "IClientPlugin.h"
+
+#include "../Common/PluginPacket.h"
+#include "../Common/CommPlugin.h"
+
+class   Network
+{
+public:
+    Network();
+    void            sendPacket(const PluginPacket& packet) const;
+
+private:
+    IClientPlugin   *plugin;
+
+};
+
 #endif // NETWORK_H
