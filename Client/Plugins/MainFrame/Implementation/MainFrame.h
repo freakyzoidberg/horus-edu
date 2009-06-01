bbc2e7bde05e673f3105c849c5c24425b71d1e40
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-06-01 17:20:19 +0000

    [[Client]] * delete bad error message while all is good when loading plugin
    [[Client]] refs #71
    * try to make the plugin compliant...

--- Client/Plugins/MainFrame/Implementation/MainFrame.h
+++ Client/Plugins/MainFrame/Implementation/MainFrame.h
@@ -7,8 +7,12 @@
 
 class MAINFRAMESHARED_EXPORT MainFrame : public IClientPlugin
 {
+    Q_OBJECT
+    Q_INTERFACES(IClientPlugin)
+
 public:
     MainFrame();
+    IClient             *client;
     const QByteArray    getName() const;
     const QByteArray    getVersion() const;
     QStringList         getPluginsConflicts() const;
