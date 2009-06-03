7dd6d5a329fe13d80885e5d77a0545a79465e262
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-06-03 14:47:34 +0000

    [[Client]] * add convenience functions in PluginManager for obtening network or displayable plugins * add IDisplayablePlugin and IDisplayable interfaces

--- trunk/Client/IDisplayable.h
+++ trunk/Client/IDisplayable.h
@@ -1,4 +1,9 @@
 #ifndef IDISPLAYABLE_H
 #define IDISPLAYABLE_H
 
+class   IDisplayable
+{
+ public:
+};
+
 #endif // IDISPLAYABLE_H
--- trunk/Client/IDisplayablePlugin.h
+++ trunk/Client/IDisplayablePlugin.h
@@ -1,4 +1,23 @@
 #ifndef IDISPLAYABLEPLUGIN_H
 #define IDISPLAYABLEPLUGIN_H
 
+#include "IDisplayable.h"
+
+//! Interface optionnal for using the display
+/*!
+ *  Each plugin implementing this interface will be able to use the display : show and use QWidgets
+ */
+class IDisplayablePlugin
+{
+    public:
+    //! Pointer to an implementation of the interface IDisplayable
+    /*!
+     *  This pointer is set by the Client while loading the plugin.
+     *  It let plugin access display ressources
+     */
+    IDisplayable    *display;
+};
+
+Q_DECLARE_INTERFACE(IDisplayablePlugin, "net.horus.Client.DisplayableInterface/1.0");
+
 #endif // IDISPLAYABLEPLUGIN_H
