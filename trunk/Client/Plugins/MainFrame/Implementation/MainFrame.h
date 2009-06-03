f816a1b4a858b23ee951ca2fac2afe31c7a2598e
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-06-03 15:17:07 +0000

    [[Client]] * rename File to InterfaceFile * add InterfaceDisplay
    [[Client]] refs #71
    * add interface IDisplayablePlugin to MainBoard plugin

--- trunk/Client/Plugins/MainFrame/Implementation/MainFrame.h
+++ trunk/Client/Plugins/MainFrame/Implementation/MainFrame.h
@@ -6,21 +6,25 @@
 #include <QtPlugin>
 
 #include "../../../IClientPlugin.h"
+#include "../../../IDisplayablePlugin.h"
 
-class MAINFRAMESHARED_EXPORT MainFrame : public IClientPlugin
+class MAINFRAMESHARED_EXPORT MainFrame : public IClientPlugin, public IDisplayablePlugin
 {
     Q_OBJECT
     Q_INTERFACES(IClientPlugin)
+    Q_INTERFACES(IDisplayablePlugin)
 
 public:
     MainFrame();
     IClient             *client;
+    IDisplayable             *display;
     const QByteArray    getName() const;
     const QByteArray    getVersion() const;
     QStringList         getPluginsConflicts() const;
     QStringList         getPluginsRequired() const;
     QStringList         getPluginsRecommended() const;
     bool                event(QEvent *event);
+    QWidget             *getWidget();
 };
 
 #endif // MAINFRAME_H
