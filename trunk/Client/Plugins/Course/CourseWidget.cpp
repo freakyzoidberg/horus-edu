693ea213357ddfe1116d5f4d8e595624e140ae82
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-06-05 16:40:40 +0000

    [[Course]]  * display pages

--- trunk/Client/Plugins/Course/CourseWidget.cpp
+++ trunk/Client/Plugins/Course/CourseWidget.cpp
@@ -28,18 +28,34 @@ CourseWidget::CourseWidget(Course *plugin): QSplitter::QSplitter()
     lesson->setAutoExpandDelay(500);
     lesson->setRootIsDecorated(false);
     this->addWidget(lesson);
+    this->page = new QWidget;
+    this->addWidget(page);
     view->setSelectionMode(QAbstractItemView::SingleSelection);
     view->setSelectionBehavior(QAbstractItemView::SelectItems);
-    connect(view->selectionModel(), SIGNAL(currentChanged(QModelIndex,QModelIndex)), this, SLOT(select(QModelIndex)));
+    connect(view->selectionModel(), SIGNAL(currentChanged(QModelIndex,QModelIndex)), this, SLOT(lessonSelected(QModelIndex)));
+    QModelIndex toto;
+//    select(toto);
 }
 
-void CourseWidget::select(const QModelIndex &item)
+void CourseWidget::lessonSelected(const QModelIndex &item)
 {
     this->file = this->plugin->fileManager->getFile(2);// need to use next line
 //    this->file = this->plugin->fileManager->getFile(item.data(42).toUInt());
     this->item = item;
-//    file->open(QIODevice::ReadOnly);
-//    connect(file, SIGNAL(readyRead()), this, SLOT(ready()));
+    file->open(QIODevice::ReadOnly);
+    connect(file, SIGNAL(readyRead()), this, SLOT(ready()));
+}
+
+void CourseWidget::pageSelected(const QModelIndex &item)
+{
+    ILessonManager *lessonManager;
+
+    lessonManager = qobject_cast<ILessonManager *>(this->plugin->client->getPlugin("LessonManager"));
+    if (lessonManager)
+    {
+        lessonManager->hideCurrentPage();
+        lessonManager->displayPage(item.data(Qt::UserRole).value<ILesson::IPage *>(), this->page);
+    }
 }
 
 void CourseWidget::ready()
@@ -48,12 +64,11 @@ void CourseWidget::ready()
 
     disconnect(file, SIGNAL(readyRead()), this, SLOT(ready()));
     this->file->close();
-    qDebug() << item.data(42);
-    qDebug() << item;
     lessonManager = qobject_cast<ILessonManager *>(this->plugin->client->getPlugin("LessonManager"));
     if (lessonManager)
     {
-//        this->lesson->setModel(lessonManager->getLesson(2));// need to use next line
+        this->lesson->setModel(lessonManager->getLesson(2));// need to use next line
 //        this->lesson->setModel(lessonManager->getLesson(item.data(42).toUInt()));
+        connect(lesson->selectionModel(), SIGNAL(currentChanged(QModelIndex,QModelIndex)), this, SLOT(pageSelected(QModelIndex)));
     }
 }
--- trunk/Client/Plugins/Course/CourseWidget.h
+++ trunk/Client/Plugins/Course/CourseWidget.h
@@ -18,12 +18,14 @@ public:
 private:
     QAbstractItemModel  *model;
     QTreeView *lesson;
+    QWidget *page;
     Course *plugin;
     IFile *file;
     QModelIndex item;
 
 private slots:
-    void select(const QModelIndex &item);
+    void lessonSelected(const QModelIndex &item);
+    void pageSelected(const QModelIndex &item);
     void ready();
 };
 #endif // COURSEWIDGET_H
