469e3602dde26c4e65dae56191b22b11f6acfde6
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-06-05 11:07:14 +0000

    [[CLient]] [[Plugin dispPDF]] * add file dispPDF_global.h

---
+++ Client/Plugins/dispPDF/dispPDF_global.h
@@ -0,0 +1,18 @@
+#ifndef DISPPDF_GLOBAL_H
+#define DISPPDF_GLOBAL_H
+
+#ifndef DISPPDF_GLOBAL_H
+#define DISPPDF_GLOBAL_H
+
+#include <QtCore/qglobal.h>
+#include <QtPlugin>
+
+#if defined(DISPPDF_LIBRARY)
+#  define DISPPDFSHARED_EXPORT Q_DECL_EXPORT
+#else
+#  define DISPPDFSHARED_EXPORT Q_DECL_IMPORT
+#endif
+
+#endif // TREEMANAGEMENT_GLOBAL_H
+
+#endif // DISPPDF_GLOBAL_H
