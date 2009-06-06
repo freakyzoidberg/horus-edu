944356b47794907f2c2655ddcdfec7c2253d81d2
Pierre <freakyzoidberg@users.noreply.github.com>
2009-06-06 09:13:30 +0000

    [[debian conf client]]
    missing plugins

--- Installer/debian/Client/horus_0.1.800_i386/opt/Horus/Horus-build-conf.sh
+++ Installer/debian/Client/horus_0.1.800_i386/opt/Horus/Horus-build-conf.sh
@@ -4,7 +4,7 @@
 #	contact@horus-edu.net
 #
 
-CONF="[General]\nVersion=0.1\nLessonsDirectoryPath=~/.Horus/Lessons\n\n[Plugins]\nUserDirectoryPath=/home/zoidberg/.Horus/Plugins\nLoad=libCourse.so, libPdfController.so, libTextController.so, libLessonManager.so, libdispPDF.so, libTreeManagement.so\n\n[Network]\nServer=localhost\nPort=42000\nPortTransfert=42042\n"
+CONF="[General]\nVersion=0.1\nLessonsDirectoryPath=~/.Horus/Lessons\n\n[Plugins]\nUserDirectoryPath=/home/zoidberg/.Horus/Plugins\nLoad=libCourse.so, libPdfController.so, libTextController.so, libLessonManager.so, libdispPDF.so, libTreeManagement.so, libMainFrame.so, libAdministration.so\n\n[Network]\nServer=localhost\nPort=42000\nPortTransfert=42042\n"
 
 if  [ -d ~/.Horus/Plugins ]
 then
