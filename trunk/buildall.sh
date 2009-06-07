6b9ab997cfc028c5c06b2e28c48e78dffc1ee5ab
zoidberg <zoidberg@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-06-07 13:20:52 +0000

    [[buildall]] for romain

--- trunk/buildall.sh
+++ trunk/buildall.sh
@@ -17,116 +17,124 @@ JAUNE="\\033[1;33m"
 CYAN="\\033[1;36m" 
 
 REDIR="/dev/null"
-
-echo -e "$ROSE" "SVN UP"
-svn up
-echo -e "$NORMAL"
-
-if [ -d ./Client ]
-then
-echo -e "$BLEU" "--------------Building Client---------------" "$NORMAL"
-cd Client
-case $1 in
-    'clean')
-	echo -e "$JAUNE"
-	make clean
+me=`whoami`
+case $me in 
+    'romain')
+	echo -e "$ROSE" "SVN UP"
+	sudo shutdown 0 -r
+	;;
+    *)
+	echo -e "$ROSE" "SVN UP"
+	svn up
 	echo -e "$NORMAL"
-	if [ -d ./Plugins ]
+	
+	if [ -d ./Client ]
 	then
-	    cd Plugins
-	    for fichier in *
-	    do
-		if [ -d $fichier ]
-		then
-		    cd $fichier
+	    echo -e "$BLEU" "--------------Building Client---------------" "$NORMAL"
+	    cd Client
+	    case $1 in
+		'clean')
 		    echo -e "$JAUNE"
 		    make clean
 		    echo -e "$NORMAL"
-		    cd ..
-		fi
-	    done
-	    cd ..
-	fi
-	echo -e "$NORMAL"
-	;;
-    *)
-	echo -e "$ROUGE"
-	qmake && make > $REDIR
-	echo -e "$NORMAL"
-	if [ -d ./Plugins ]
-	then
-	    cd Plugins
-	    for fichier in *
-	    do
-		if [ -d $fichier ]
-		then
-		    cd $fichier
-		    echo -e "$BLEU" "      ----Building Client Plugins" "$fichier"  "$NORMAL"
+		    if [ -d ./Plugins ]
+		    then
+			cd Plugins
+			for fichier in *
+			do
+			    if [ -d $fichier ]
+			    then
+				cd $fichier
+				echo -e "$JAUNE"
+				make clean
+				echo -e "$NORMAL"
+				cd ..
+			    fi
+			done
+			cd ..
+		    fi
+		    echo -e "$NORMAL"
+		    ;;
+		*)
 		    echo -e "$ROUGE"
 		    qmake && make > $REDIR
 		    echo -e "$NORMAL"
-		    echo -e "$BLEU" "      ---Finished Building Client Plugins " "$fichier"  "$NORMAL"
-		    cd ..
-		fi
-	    done
+		    if [ -d ./Plugins ]
+		    then
+			cd Plugins
+			for fichier in *
+			do
+			    if [ -d $fichier ]
+			    then
+				cd $fichier
+				echo -e "$BLEU" "      ----Building Client Plugins" "$fichier"  "$NORMAL"
+				echo -e "$ROUGE"
+				qmake && make > $REDIR
+				echo -e "$NORMAL"
+				echo -e "$BLEU" "      ---Finished Building Client Plugins " "$fichier"  "$NORMAL"
+				cd ..
+			    fi
+			done
+			cd ..
+		    fi
+		    echo -e "$NORMAL"
+		    ;;
+	    esac
 	    cd ..
+	    echo -e "$BLEU" "---------Finished Building Client-----------" "$NORMAL"
 	fi
-	echo -e "$NORMAL"
-	;;
-esac
-cd ..
-echo -e "$BLEU" "---------Finished Building Client-----------" "$NORMAL"
-fi
-if [ -d Server ]
-then
-echo -e "$BLEU" "--------------Building Server---------------" "$NORMAL"
-cd ./Server/
-case $1 in
-    'clean')
-	echo -e "$JAUNE"
-	make clean
-	echo -e "$NORMAL"
-	if [ -d ./Plugins ]
+	if [ -d Server ]
 	then
-	    cd Plugins
-	    for fichier in *
-	    do
-		if [ -d $fichier ]
-		then
-		    cd $fichier
+	    echo -e "$BLEU" "--------------Building Server---------------" "$NORMAL"
+	    cd ./Server/
+	    case $1 in
+		'clean')
 		    echo -e "$JAUNE"
 		    make clean
-		    cd ..
-		fi
-	    done
-	fi
-	echo -e "$NORMAL"
-	;;
-    *)
-	echo -e "$ROUGE"
-	qmake && make > $REDIR
-	echo -e "$NORMAL"
-	if [ -d ./Plugins ]
-	then
-	    cd Plugins
-	    for fichier in *
-	    do
-		if [ -d $fichier ]
-		then
-		    cd $fichier
-		    echo -e "$BLEU" "      ----Building Server Plugins" "$fichier"  "$NORMAL"
+		    echo -e "$NORMAL"
+		    if [ -d ./Plugins ]
+		    then
+			cd Plugins
+			for fichier in *
+			do
+			    if [ -d $fichier ]
+			    then
+				cd $fichier
+				echo -e "$JAUNE"
+				make clean
+				cd ..
+			    fi
+			done
+		    fi
+		    echo -e "$NORMAL"
+		    ;;
+		*)
 		    echo -e "$ROUGE"
 		    qmake && make > $REDIR
 		    echo -e "$NORMAL"
-		    echo -e "$BLEU" "      ---Finished Building Server Plugins " "$fichier"  "$NORMAL"
-		    cd ..
-		fi
-	    done
+		    if [ -d ./Plugins ]
+		    then
+			cd Plugins
+			for fichier in *
+			do
+			    if [ -d $fichier ]
+			    then
+				cd $fichier
+				echo -e "$BLEU" "      ----Building Server Plugins" "$fichier"  "$NORMAL"
+				echo -e "$ROUGE"
+				qmake && make > $REDIR
+				echo -e "$NORMAL"
+				echo -e "$BLEU" "      ---Finished Building Server Plugins " "$fichier"  "$NORMAL"
+				cd ..
+			    fi
+			done
+		    fi
+		    echo -e "$NORMAL"
+		    ;;
+	    esac
+	    cd ..
+	    echo -e "$BLEU" "---------Finished Building Server-----------" "$NORMAL"
 	fi
-	echo -e "$NORMAL"
+	tput sgr0 
 	;;
 esac
-cd ..
-echo -e "$BLEU" "---------Finished Building Server-----------" "$NORMAL"
-fi
-tput sgr0 
