1e5feccacfd9173194abe9dd00e782d3e5329559
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-08-17 16:40:03 +0000

    [Client] * wrong commit last time

--- trunk/Client/Plugins/editPDF/editpdf.h
+++ trunk/Client/Plugins/editPDF/editpdf.h
@@ -3,14 +3,15 @@
 
 #include <QStringList>
 #include <QWidget>
-
+#include <QDomNode>
+#include <QTreeWidgetItem>
 #include "editPDF_global.h"
 
 class EDITPDFSHARED_EXPORT EditPDF
 {
     public:
         EditPDF();
-        QWidget *getTocInWidget();
+        QWidget *getTocInWidget(QDomNode *, QTreeWidgetItem *);
 
     private:
         QStringList     pluginsRequired;
