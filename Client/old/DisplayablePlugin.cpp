98041d866d3c53449bd9eb4e3bc5f8bf71a6ab2d
grande_a <grande_a@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-06 18:48:57 +0000

    "old" folder useless (svn ça sert a ça bordel !)

--- Client/old/DisplayablePlugin.cpp
+++
@@ -1,7 +0,0 @@
-#include "DisplayablePlugin.h"
-#include "ClientApplication.h"
-
-void    DisplayablePlugin::setCentralWidget(QWidget *widget)
-{
-    return (((ClientApplication*)(QCoreApplication::instance()))->mainWindow->setCentralWidget(widget));
-}
