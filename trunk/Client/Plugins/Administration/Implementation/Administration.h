034e48a92d7e8787f32730bef0fbb98fc12f7d04
abderman <abderman@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-06 08:37:32 +0000

    [[Client]] * Fichiers oubliés

---
+++ trunk/Client/Plugins/Administration/Implementation/Administration.h
@@ -0,0 +1,43 @@
+#ifndef __ADMINISTRATION_H__
+# define __ADMINISTRATION_H__
+
+# include <QEvent>
+# include <QtPlugin>
+# include <QObject>
+
+# include "../../../NetworkPlugin.h"
+# include "../../../DisplayablePlugin.h"
+
+# include "AdministrationFrame.h"
+
+class Administration : public NetworkPlugin, public DisplayablePlugin
+{
+    Q_OBJECT
+    Q_INTERFACES(NetworkPluginClient)
+    Q_INTERFACES(DisplayablePlugin)
+
+public:
+        //! return the name of the plugin
+         /*!
+            \return the variable modName
+         */
+         const QString       pluginName() const;
+
+         //! return the version of the plugin
+         /*!
+           \return the variable modVersion
+         */
+         const QString       pluginVersion() const;
+		 void load();
+		 void unload();
+        void                    receivePacket(UserData* user, const PluginPacket&);
+        QWidget                 *getWidget();
+
+private:
+        AdministrationFrame           *widget;
+
+private slots:
+	 void send(PluginPacket *packet);
+};
+
+#endif // ADMINISTRATION_H
