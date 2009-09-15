2d7a2d6042f5ed9de068f69526f3e2a8ad57f90c
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-15 20:18:58 +0000

    [[LessonManager]] * resolve include issues

--- Client/Plugins/Administration/Implementation/TeacherModel.h
+++ Client/Plugins/Administration/Implementation/TeacherModel.h
@@ -11,21 +11,21 @@ class TeacherModel: public QAbstractItemModel
      Q_OBJECT
 
  public:
-     TeacherModel(const QString &data, QObject *parent = 0);
-     ~TeacherModel();
+ //    TeacherModel(const QString &data, QObject *parent = 0);
+ //    ~TeacherModel();
 
-     QVariant data(const QModelIndex &index, int role) const;
-     Qt::ItemFlags flags(const QModelIndex &index) const;
-     QVariant headerData(int section, Qt::Orientation orientation,
-                         int role = Qt::DisplayRole) const;
-     QModelIndex index(int row, int column,
-                       const QModelIndex &parent = QModelIndex()) const;
-     QModelIndex parent(const QModelIndex &index) const;
-     int rowCount(const QModelIndex &parent = QModelIndex()) const;
-     int columnCount(const QModelIndex &parent = QModelIndex()) const;
+ //    QVariant data(const QModelIndex &index, int role) const;
+ //    Qt::ItemFlags flags(const QModelIndex &index) const;
+ //    QVariant headerData(int section, Qt::Orientation orientation,
+ //                        int role = Qt::DisplayRole) const;
+ //    QModelIndex index(int row, int column,
+ //                      const QModelIndex &parent = QModelIndex()) const;
+ //    QModelIndex parent(const QModelIndex &index) const;
+ //    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+ //    int columnCount(const QModelIndex &parent = QModelIndex()) const;
 
- private:
-     void setupModelData(const QStringList &lines);
+ //private:
+ //    void setupModelData(const QStringList &lines);
 };
 
 #endif // TEACHERMODEL_H
