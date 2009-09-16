6bb4b3bd23fc9c0c0d81ad278faf061809cc317c
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-16 15:14:04 +0000

    [[Course]] * try to resize Items

--- trunk/Client/Plugins/Course/Implementation/CourseWidget.cpp
+++ trunk/Client/Plugins/Course/Implementation/CourseWidget.cpp
@@ -21,11 +21,6 @@ CourseWidget::CourseWidget(ILessonManager *_lessonPlugin, TreeDataPlugin *_treeP
     leftPane->setLayout(layout);
     this->pageWidget = new WhiteBoard(controllers);
     this->addWidget(this->pageWidget);
-
-    Items *test = new Items(this->pageWidget);
-    Items *test2 = new Items(this->pageWidget);
-
-    test2->move(0,0);
 }
 
 
