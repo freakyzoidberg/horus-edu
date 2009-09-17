46ce5bc3ff0043d9a1d37eb124b978d870ff6744
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-17 04:05:51 +0000

    [frg]

--- Client/Plugins/PdfDisplayer/Implementation/PdfFile.cpp
+++ Client/Plugins/PdfDisplayer/Implementation/PdfFile.cpp
@@ -205,7 +205,8 @@ bool        PdfFile::loadPage(unsigned int pageNb)
     }
     else if (pageNb != currentPageNb)
     {
-        delete currentPage;
+        if (currentPage)
+            delete currentPage;
         currentPage = pdfDoc->page(pageNb);
         currentPageNb = pageNb;
     }
