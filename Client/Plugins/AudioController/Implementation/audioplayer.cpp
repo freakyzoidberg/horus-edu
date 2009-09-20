868c02870aaeb4e3576c96c1c8e43deef742ff79
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-20 23:34:20 +0000

    [[Client]] * add some icons for player

--- Client/Plugins/AudioController/Implementation/audioplayer.cpp
+++ Client/Plugins/AudioController/Implementation/audioplayer.cpp
@@ -1,16 +1,21 @@
 #include "audioplayer.h"
 
+#include <QIcon>
+
 AudioPlayer::AudioPlayer(QWidget *parent, Phonon::MediaObject *media) : QWidget(parent)
 {
     seekSlider = new Phonon::SeekSlider(this);
     volumeSlider = new Phonon::VolumeSlider(this);
     volumeSlider->setGeometry(0, 21, 100, 20);
     seekSlider->setGeometry(46, 0, 100, 20);
-    stop = new QPushButton("S", this);
+    stop = new QPushButton(this);
+    stop->setIcon(QIcon(":/stop.png"));
     stop->setGeometry(101, 21, 20, 20);
-    pause = new QPushButton("P", this);
+    pause = new QPushButton(this);
+    pause->setIcon(QIcon(":/pause.png"));
     pause->setGeometry(122, 21, 20, 20);
-    play = new QPushButton("R", this);
+    play = new QPushButton(this);
+    play->setIcon(QIcon(":/play.png"));
     play->setGeometry(143, 21, 20, 20);
     this->setMinimumWidth(200);
     connect(play, SIGNAL(clicked()), media, SLOT(play()));
