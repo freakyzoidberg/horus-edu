74ce359ca0e66d7eb98789a99ee7f5ee79e01e5e
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-21 13:33:08 +0000

    [[Client]] * add files player.*

---
+++ Client/Plugins/MediaController/Implementation/player.h
@@ -0,0 +1,38 @@
+#ifndef AUDIOPLAYER_H
+#define AUDIOPLAYER_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QLCDNumber>
+#include <phonon/volumeslider.h>
+#include <phonon/seekslider.h>
+#include <phonon/mediaobject.h>
+#include "phonon/videoplayer.h"
+
+class Player : public QWidget
+{
+    Q_OBJECT
+public:
+    Player(QWidget *);
+    ~Player();
+    Phonon::VolumeSlider    *getVolumeSlider();
+    Phonon::SeekSlider      *getSeekSlider();
+    Phonon::MediaObject     *getMedia();
+    QPushButton             *getStop();
+    QPushButton             *getPause();
+    QPushButton             *getPlay();
+    Phonon::VideoPlayer     *getVidPlayer();
+
+public slots:
+    void                    tick(qint64);
+
+private:
+    Phonon::SeekSlider      *seekSlider;
+    Phonon::VolumeSlider    *volumeSlider;
+    Phonon::MediaObject     *media;
+    Phonon::VideoPlayer     *vidPlayer;
+    QPushButton             *stopV, *pauseV, *playV;
+    QLCDNumber              *timeLCD;
+};
+
+#endif // AUDIOPLAYER_H
