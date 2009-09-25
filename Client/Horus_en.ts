876a11c3e2e589164e8c71585419da2dab1b1fef
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-25 16:17:45 +0000

    [[Client]] refs #142 * load language files * add translation files for core * add Locale in settings

--- Client/Horus_en.ts
+++ Client/Horus_en.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="en_US">
 <context>
     <name>Debugger</name>
     <message>
         <location filename="Debugger.cpp" line="15"/>
         <source>Horus debugger</source>
-        <translation type="unfinished"></translation>
+        <translation>Horus - Debugger</translation>
     </message>
 </context>
 <context>
@@ -14,22 +14,22 @@
     <message>
         <location filename="Ui/SettingsDialog.ui" line="14"/>
         <source>Horus - Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Horus - Settings</translation>
     </message>
     <message>
         <location filename="Ui/SettingsDialog.ui" line="28"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>General</translation>
     </message>
     <message>
         <location filename="Ui/SettingsDialog.ui" line="33"/>
         <source>Plugins</source>
-        <translation type="unfinished"></translation>
+        <translation>Plugins</translation>
     </message>
     <message>
         <location filename="Ui/SettingsDialog.ui" line="38"/>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Network</translation>
     </message>
 </context>
 <context>
@@ -37,22 +37,22 @@
     <message>
         <location filename="Ui/LoginDialog.ui" line="14"/>
         <source>Fenetre de connexion</source>
-        <translation type="unfinished"></translation>
+        <translation>Horus - Connection</translation>
     </message>
     <message>
         <location filename="Ui/LoginDialog.ui" line="32"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Connect</translation>
     </message>
     <message>
         <location filename="Ui/LoginDialog.ui" line="68"/>
         <source>Login:</source>
-        <translation type="unfinished"></translation>
+        <translation>Username:</translation>
     </message>
     <message>
         <location filename="Ui/LoginDialog.ui" line="81"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Password:</translation>
     </message>
 </context>
 <context>
@@ -60,342 +60,342 @@
     <message>
         <location filename="MainWindow.cpp" line="30"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished"></translation>
+        <translation>E&amp;xit</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="31"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="32"/>
         <source>Exit the application</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit the application</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="35"/>
         <source>&amp;Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Settings</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="36"/>
         <source>Ctrl+T</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+T</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="37"/>
         <source>Edit settings of Horus</source>
-        <translation type="unfinished"></translation>
+        <translation>Edit settings</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="43"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;File</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="44"/>
         <source>&amp;Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Edit</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="48"/>
         <source>New...</source>
-        <translation type="unfinished"></translation>
+        <translation>New...</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="49"/>
         <source>Ctrl+N</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+N</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="51"/>
         <source>Load...</source>
-        <translation type="unfinished"></translation>
+        <translation>Load...</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="52"/>
         <source>Ctrl+L</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+L</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="54"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Save</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="56"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+S</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="57"/>
         <source>Save As...</source>
-        <translation type="unfinished"></translation>
+        <translation>Save As...</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="59"/>
         <source>Save All</source>
-        <translation type="unfinished"></translation>
+        <translation>Save All</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="64"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Close</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="65"/>
         <source>Ctrl+W</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+W</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="67"/>
         <source>Close All</source>
-        <translation type="unfinished"></translation>
+        <translation>Close All</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="72"/>
         <source>Page Setup...</source>
-        <translation type="unfinished"></translation>
+        <translation>Page Setup...</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="73"/>
         <source>Ctrl+E</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+E</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="75"/>
         <source>Print...</source>
-        <translation type="unfinished"></translation>
+        <translation>Print...</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="76"/>
         <source>Ctrl+P</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+P</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="78"/>
         <source>Print Preview...</source>
-        <translation type="unfinished"></translation>
+        <translation>Print Preview...</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="83"/>
         <source>Undo</source>
-        <translation type="unfinished"></translation>
+        <translation>Undo</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="84"/>
         <source>Ctrl+U</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+U</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="86"/>
         <source>Redo</source>
-        <translation type="unfinished"></translation>
+        <translation>Redo</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="87"/>
         <source>Ctrl+R</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+R</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="92"/>
         <source>Cut</source>
-        <translation type="unfinished"></translation>
+        <translation>Cut</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="93"/>
         <source>Ctrl+C</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+C</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="95"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Copy</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="96"/>
         <source>Ctrl+V</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+V</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="98"/>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation>Paste</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="99"/>
         <source>Ctrl+X</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+X</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="101"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Delete</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="106"/>
         <source>Select All</source>
-        <translation type="unfinished"></translation>
+        <translation>Select All</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="107"/>
         <source>Ctrl+A</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+A</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="112"/>
         <source>Replace...</source>
-        <translation type="unfinished"></translation>
+        <translation>Replace...</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="113"/>
         <source>Ctrl+F</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+F</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="118"/>
         <source>&amp;View</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;View</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="119"/>
         <source>Main Board</source>
-        <translation type="unfinished"></translation>
+        <translation>Main Board</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="120"/>
         <source>Alt+H</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+H</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="122"/>
         <source>Course</source>
-        <translation type="unfinished"></translation>
+        <translation>Course</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="123"/>
         <source>Alt+C</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+C</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="125"/>
         <source>Marks</source>
-        <translation type="unfinished"></translation>
+        <translation>Marks</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="126"/>
         <source>Alt+M</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+M</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="128"/>
         <source>Calendar</source>
-        <translation type="unfinished"></translation>
+        <translation>Calendar</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="129"/>
         <source>Alt+L</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+L</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="131"/>
         <source>Administration</source>
-        <translation type="unfinished"></translation>
+        <translation>Administration</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="132"/>
         <source>Alt+A</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+A</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="134"/>
         <source>&amp;Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Tools</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="135"/>
         <source>Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Editor</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="137"/>
         <source>Remote</source>
-        <translation type="unfinished"></translation>
+        <translation>Remote</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="142"/>
         <source>Debugger</source>
-        <translation type="unfinished"></translation>
+        <translation>Debugger</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="143"/>
         <source>Ctrl+G</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+G</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="145"/>
         <source>Console</source>
-        <translation type="unfinished"></translation>
+        <translation>Console</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="147"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Help</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="148"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Search</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="149"/>
         <source>Ctrl+F1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+F1</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="151"/>
         <source>Content</source>
-        <translation type="unfinished"></translation>
+        <translation>Content</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="152"/>
         <source>Ctrl+F2</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+F2</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="154"/>
         <source>Index</source>
-        <translation type="unfinished"></translation>
+        <translation>Index</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="155"/>
         <source>Ctrl+F3</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+F3</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="160"/>
         <source>?</source>
-        <translation type="unfinished"></translation>
+        <translation>?</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="161"/>
         <source>Ctrl+?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+?</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="163"/>
         <source>Report Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Report Bug</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="168"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>About</translation>
     </message>
     <message>
         <location filename="Ui/MainWindow.ui" line="14"/>
         <source>Horus</source>
-        <translation type="unfinished"></translation>
+        <translation>Horus</translation>
     </message>
 </context>
 <context>
@@ -403,7 +403,7 @@
     <message>
         <location filename="Ui/Loader.ui" line="35"/>
         <source>Horus - Loading</source>
-        <translation type="unfinished"></translation>
+        <translation>Horus - Loading</translation>
     </message>
 </context>
 <context>
@@ -411,7 +411,7 @@
     <message>
         <location filename="Ui/DockMenu.ui" line="45"/>
         <source>Menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Menu</translation>
     </message>
 </context>
 </TS>
