2d67fbb5279615b3180365a388957a2ac210d452
Pierre <freakyzoidberg@users.noreply.github.com>
2009-09-27 15:45:05 +0000

    [[Mail]] Mail Data item ok

--- Common/DataImplementations/MailData/MailItem.h
+++ Common/DataImplementations/MailData/MailItem.h
@@ -6,44 +6,40 @@
 class MailItem
 {
 public:
-        inline MailItem(quint32 id, quint32 left, quint32 top, quint32 width, quint32 height,      bool isDocked)
-							{ _id=id;   _left=left;    _top=top;  _width=width; _height=height; _isDocked=isDocked; }
+        inline MailItem(quint32 id, QStringList dest, QStringList cc, QStringList bcc, QString content)
+                                                        { _id=id;   _dest=dest;    _cc=cc;  _bcc=bcc; _content=content;}
 
         inline MailItem(QDataStream& s) { *this << s; }
 
 	inline int			id()     const { return _id; }
-	inline int			left()   const { return _left; }
-	inline int			top()    const { return _top; }
-	inline int			width()  const { return _width; }
-	inline int			height() const { return _height; }
-	inline bool			docked() const { return _isDocked; }
+        inline QStringList			dest()   const { return _dest; }
+        inline QStringList			cc()    const { return _cc; }
+        inline QStringList			bcc()  const { return _bcc; }
+        inline QString			content() const { return _content; }
 
 	inline QDataStream& operator>>(QDataStream& s) const
 	{ return s
 	  << _id
-	  << _left
-	  << _top
-	  << _width
-	  << _height
-	  << _isDocked
+          << _dest
+          << _cc
+          << _bcc
+          << _content
 	;}
 	inline QDataStream& operator<<(QDataStream& s)
 	{ return s
 	  >> _id
-	  >> _left
-	  >> _top
-	  >> _width
-	  >> _height
-	  >> _isDocked
+          >> _dest
+          >> _cc
+          >> _bcc
+          >> _content
 	 ;}
 
 private:
 	quint32				_id;
-	quint32				_left;
-	quint32				_top;
-	quint32				_width;
-	quint32				_height;
-	bool				_isDocked;
+        QStringList				_dest;
+        QStringList				_cc;
+        QStringList				_bcc;
+        QString				_content;
 };
 
 typedef QList<MailItem> MailItemList;
