a9a5205f34b77100e3235e96c3bacf04cab4dd30
abderman <abderman@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-29 22:55:16 +0000

    [[Client]] * solve conflict

--- trunk/Client/Plugins/Administration/Implementation/FilterModel.h
+++ trunk/Client/Plugins/Administration/Implementation/FilterModel.h
@@ -5,14 +5,18 @@
 #include "../../../../Common/TreeData.h"
 #include "../../../../Common/UserData.h"
 
-class FilterModel : public QSortFilterProxyModel
+class FilterModel : public QAbstractProxyModel
 {
     Q_OBJECT
     public:
         FilterModel(int _type, QObject *parent = 0);
     private:
-        QModelIndex mapFromSource (const QModelIndex & sourceIndex);
-        bool        filterAcceptsRow(int source_row, const QModelIndex & source_parent);
+        QModelIndex index(int row, int column, const QModelIndex &parent=QModelIndex()) const;
+        QModelIndex parent(const QModelIndex &index) const;
+        QModelIndex mapToSource(const QModelIndex &proxyIndex) const;
+        QModelIndex mapFromSource(const QModelIndex &sourceIndex) const;
+        int         rowCount(const QModelIndex &index=QModelIndex()) const;
+        int         columnCount(const QModelIndex &index=QModelIndex()) const;
         int         type;
 };
 
