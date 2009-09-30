e5eb4165aade46b9a54ad402385f4523cf966fc3
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-09-30 18:50:32 +0000

    [[Course]] refs #141 * filter model

--- Client/Plugins/Course/Implementation/JoinWhiteBoard.cpp
+++ Client/Plugins/Course/Implementation/JoinWhiteBoard.cpp
@@ -1,9 +1,16 @@
 #include	"JoinWhiteBoard.h"
 
+#include	<QSortFilterProxyModel>
+
 #include	"WhiteBoardModel.h"
 
 JoinWhiteBoard::JoinWhiteBoard(PluginManager *pluginManager) : QWidget()
 {
 	this->ui.setupUi(this);
-	this->ui.treeView->setModel(new WhiteBoardModel(pluginManager));
+	QAbstractItemModel		*model = new WhiteBoardModel(pluginManager);
+    QSortFilterProxyModel	*proxyModel = new QSortFilterProxyModel(this);
+	proxyModel->setFilterRegExp(QRegExp("\\b(ROOT|CLASSES|GRADE|SUBJECT)\\b", Qt::CaseSensitive, QRegExp::RegExp));
+	proxyModel->setFilterKeyColumn(1);
+	proxyModel->setSourceModel(model);
+	this->ui.treeView->setModel(proxyModel);
 }
