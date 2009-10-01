e6b1474503fc62202364c16047651b6a0281418f
grande_a <grande_a@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-10-01 09:12:32 +0000

    [[Client]]
    * Debut de createLesson
    * Ptite modif dans le stylesheet pour les treeviews

--- trunk/Client/Plugins/Course/Course_fr.ts
+++ trunk/Client/Plugins/Course/Course_fr.ts
@@ -4,9 +4,88 @@
 <context>
     <name>Course</name>
     <message>
-        <location filename="Implementation/Course.cpp" line="16"/>
+        <location filename="Implementation/Course.cpp" line="23"/>
         <source>Course</source>
         <translation>Cours</translation>
+    </message>
+    <message>
+        <location filename="Implementation/Course.cpp" line="92"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CreateForm</name>
+    <message>
+        <location filename="Implementation/Ui/CreateWhiteBoard.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/Ui/CreateWhiteBoard.ui" line="47"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/Ui/CreateWhiteBoard.ui" line="54"/>
+        <source>Sync type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/Ui/CreateWhiteBoard.ui" line="67"/>
+        <source>Create</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CreateWhiteBoard</name>
+    <message>
+        <location filename="Implementation/CreateWhiteBoard.cpp" line="27"/>
+        <source>No Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/CreateWhiteBoard.cpp" line="28"/>
+        <source>Semi Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/CreateWhiteBoard.cpp" line="29"/>
+        <source>Full Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/CreateWhiteBoard.cpp" line="52"/>
+        <source>Please select only one place to create the whiteboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/CreateWhiteBoard.cpp" line="55"/>
+        <source>Please select a place to create the whiteboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/CreateWhiteBoard.cpp" line="58"/>
+        <source>Please use a sync mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/CreateWhiteBoard.cpp" line="61"/>
+        <source>Please fill a name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JoinForm</name>
+    <message>
+        <location filename="Implementation/Ui/JoinWhiteBoard.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Implementation/Ui/JoinWhiteBoard.ui" line="28"/>
+        <source>Join</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -28,14 +107,14 @@
 <context>
     <name>WhiteBoard</name>
     <message>
-        <location filename="Implementation/WhiteBoard.cpp" line="102"/>
-        <location filename="Implementation/WhiteBoard.cpp" line="187"/>
+        <location filename="Implementation/WhiteBoard.cpp" line="122"/>
+        <location filename="Implementation/WhiteBoard.cpp" line="207"/>
         <source>WhiteBoard::dropEvent: unable to find a controller for</source>
         <translation>WhiteBoard::dropEvent: impossible de trouver un controlleur pour le type</translation>
     </message>
     <message>
-        <location filename="Implementation/WhiteBoard.cpp" line="102"/>
-        <location filename="Implementation/WhiteBoard.cpp" line="187"/>
+        <location filename="Implementation/WhiteBoard.cpp" line="122"/>
+        <location filename="Implementation/WhiteBoard.cpp" line="207"/>
         <source>type.</source>
         <translation>.</translation>
     </message>
@@ -43,12 +122,12 @@
 <context>
     <name>WhiteBoardData</name>
     <message>
-        <location filename="../../../Common/DataImplementations/WhiteBoardData/WhiteBoardData.cpp" line="35"/>
+        <location filename="../../../Common/DataImplementations/WhiteBoardData/WhiteBoardData.cpp" line="51"/>
         <source>WhiteBoardData::</source>
         <translation>WhiteBoardData::</translation>
     </message>
     <message>
-        <location filename="../../../Common/DataImplementations/WhiteBoardData/WhiteBoardData.cpp" line="47"/>
+        <location filename="../../../Common/DataImplementations/WhiteBoardData/WhiteBoardData.cpp" line="63"/>
         <source>White Board</source>
         <translation>Tableau Blanc</translation>
     </message>
