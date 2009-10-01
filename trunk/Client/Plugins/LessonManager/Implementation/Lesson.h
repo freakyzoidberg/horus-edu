3c412815668bc6a96a4c588c004dc6ef42279ffa
git <git@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-10-01 14:40:48 +0000

    encor pour adri

--- trunk/Client/Plugins/LessonManager/Implementation/Lesson.h
+++ trunk/Client/Plugins/LessonManager/Implementation/Lesson.h
@@ -24,6 +24,8 @@ public:
     bool    endElement(const QString &namespaceURI, const QString &localName, const QString &qName);
     bool    characters(const QString& ch);
 
+	inline int getId() { return _fileData->id(); }
+
 	FileData *getFiledata() { return _fileData; }
 
 private:
