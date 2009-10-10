cace5d60189b2e27343d8de3ab3d94c43fbbd062
abderman <abderman@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-10-10 22:22:22 +0000

    [[Client]] * add some files

---
+++ trunk/Client/Plugins/Administration/Implementation/GradesFrame.cpp
@@ -0,0 +1,5 @@
+#include "GradesFrame.h"
+
+GradesFrame::GradesFrame()
+{
+}
---
+++ trunk/Client/Plugins/Administration/Implementation/ReportFrame.h
@@ -0,0 +1,12 @@
+#ifndef REPORTFRAME_H
+#define REPORTFRAME_H
+
+#include <QWidget>
+
+class ReportFrame : public QWidget
+{
+public:
+    ReportFrame();
+};
+
+#endif // REPORTFRAME_H
