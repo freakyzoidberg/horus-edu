d8524f0b3792a7d8b5f4c352174c72d6bd0060a2
abderman <abderman@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-10-25 14:41:25 +0000

    [[Client]] * some minor modif

--- Client/Plugins/Administration/Implementation/AbsenceFrame.cpp
+++ Client/Plugins/Administration/Implementation/AbsenceFrame.cpp
@@ -7,6 +7,18 @@ AbsenceFrame::AbsenceFrame()
 
 void    AbsenceFrame::setupUi()
 {
-
+    absLayout = new QHBoxLayout(this);
+    rightSide = new QGridLayout();
+    leftSide = new QVBoxLayout();
+    userName = new QLineEdit();
+    leftSide->addWidget(userName);
+    classList = new QComboBox();
+    leftSide->addWidget(classList);
+    userList = new QListView();
+    leftSide->addWidget(userList);
+    absLayout->addLayout(leftSide);
+    //dateAbsence;
+    //allCourse;
+    //okButton;
 
 }
