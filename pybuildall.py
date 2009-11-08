911c52e0baf31bb4bfddcb7173182908fa5a6ceb
Pierre <freakyzoidberg@users.noreply.github.com>
2009-11-08 23:54:44 +0000

    tools buildall en python pour ceux sous windows a les meme fonctionalite que le sh a tester sous windows

---
+++ pybuildall.py
@@ -0,0 +1,56 @@
+import os
+import os.path
+import shutil
+import getpass
+
+print "Hey " + getpass.getuser()
+
+if getpass.getuser() == "zoidberg":
+    MV_CLIENT_PLUGINS_TO="/home/zoidberg/.Horus/Plugins/"
+    MV_SERVER_PLUGINS_TO=""
+elif getpass.getuser() == "toto":
+    MV_CLIENT_PLUGINS_TO="/home/zoidberg/.Horus/Plugins/"
+    MV_SERVER_PLUGINS_TO=""
+else:
+    MV_CLIENT_PLUGINS_TO=""
+    MV_SERVER_PLUGINS_TO=""
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.ENDC = ''
+
+
+def ParseCompileCopy(fromwhere, towhere):
+    print bcolors.HEADER + "Compiling " + fromwhere + bcolors.ENDC
+    os.chdir(fromwhere)
+    os.system("qmake")
+    os.system("make")
+    os.chdir("Plugins")
+    flag = 0
+    
+    for filename in os.listdir ("./"):
+        if os.path.isdir (os.path.join (filename)) and filename != ".svn":
+            print bcolors.OKBLUE + "Compiling " + filename + bcolors.ENDC
+            os.chdir(filename)
+            os.system("qmake")
+            os.system("make")
+            if towhere != "":
+                for pluginfiles in os.listdir ("./"):
+                    if pluginfiles.endswith(".so") or pluginfiles.endswith(".dll"):
+                        print bcolors.OKGREEN + "Copying " + pluginfiles + bcolors.ENDC
+                        shutil.copy (pluginfiles,towhere)
+            os.chdir("..")
+    os.chdir("../..")
+    return
+
+ParseCompileCopy("./Client", MV_CLIENT_PLUGINS_TO)
+ParseCompileCopy("./Server", MV_SERVER_PLUGINS_TO)
