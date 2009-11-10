e92fe2620a5c3579f11b1bfeb32913fc38d2db82
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-10 11:10:03 +0000

    [[MainFrame]] fix #186 * added API SmallDisplayablePlugin * added example using SmallDisplayablePlugin

---
+++ trunk/Client/Plugins/MainFrame/Implementation/ExampleSmallDisplayable.cpp
@@ -0,0 +1,28 @@
+#include		"ExampleSmallDisplayable.h"
+
+#include		<QLabel>
+
+const QString	ExampleSmallDisplayable::pluginName() const
+{
+	return ("ExampleSmallDisplayable");
+}
+
+const QString	ExampleSmallDisplayable::pluginVersion() const
+{
+	return ("0.1");
+}
+
+QWidget			*ExampleSmallDisplayable::getWidget()
+{
+	return (new QLabel(tr("This is an example widget, you can drag & drop me !")));
+}
+
+const QString	ExampleSmallDisplayable::getDisplayableName() const
+{
+	return (tr("Example Widget"));
+}
+
+QIcon			ExampleSmallDisplayable::getIcon() const
+{
+	return (QIcon(":icon"));
+}
---
+++ trunk/Client/Plugins/MainFrame/Implementation/ExampleSmallDisplayable.h
@@ -0,0 +1,21 @@
+#ifndef				__EXAMPLESMALLDISPLAYABLE_H__
+# define			__EXAMPLESMALLDISPLAYABLE_H__
+
+# include			"../SmallDisplayable.h"
+
+class				ExampleSmallDisplayable : public SmallDisplayablePlugin
+{
+	Q_OBJECT
+	Q_INTERFACES(SmallDisplayablePlugin)
+
+public:
+	// Interface Plugin
+	const QString	pluginName() const;
+	const QString	pluginVersion() const;
+	// Interface SmallDisplayablePlugin
+	QWidget			*getWidget();
+	const QString	getDisplayableName() const;
+	QIcon			getIcon() const;
+};
+
+#endif
