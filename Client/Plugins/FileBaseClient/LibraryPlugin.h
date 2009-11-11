0462228fb53d6220366c709d03b43daaaaa98fb8
grande_a <grande_a@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-11 20:22:29 +0000

    [[Client]]
    * Added library icon.

--- Client/Plugins/FileBaseClient/LibraryPlugin.h
+++ Client/Plugins/FileBaseClient/LibraryPlugin.h
@@ -15,7 +15,7 @@ public:
 
 	inline QWidget*			getWidget()	{ return new LibraryWidget(pluginManager); }
 	inline const QString	getDisplayableName() const { return tr("Library"); }
-//	inline QIcon getIcon() const { return (QIcon()); }
+	inline QIcon getIcon() const { return (QIcon(":Icons/lib-icon.png")); }
 //	inline int getOrder() const { return (50); }
 //	inline bool operator<(DisplayablePlugin &right) const { return (this->getOrder() < right.getOrder()); }
 };
--- Client/Plugins/FileBaseClient/TransfertPlugin.h
+++ Client/Plugins/FileBaseClient/TransfertPlugin.h
@@ -15,7 +15,7 @@ public:
 
 	inline QWidget*			getWidget()	{ return new TransfertList; }
 	inline const QString	getDisplayableName() const { return tr("File transferts"); }
-//	inline QIcon getIcon() const { return (QIcon()); }
+//	inline QIcon getIcon() const { return (QIcon("")); }
 //	inline int getOrder() const { return (50); }
 //	inline bool operator<(DisplayablePlugin &right) const { return (this->getOrder() < right.getOrder()); }
 };
