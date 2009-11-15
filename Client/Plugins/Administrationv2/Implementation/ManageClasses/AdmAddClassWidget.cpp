08a9befe9d4b783bbcd75c5d0c44d2489e86d826
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-15 12:16:45 +0000

    [[Client]] [[Administration]] [[Manage Classes]] * add the diretory * add the class used to add user

---
+++ Client/Plugins/Administrationv2/Implementation/ManageClasses/AdmAddClassWidget.cpp
@@ -0,0 +1,5 @@
+#include "AdmAddClassWidget.h"
+
+AdmAddClassWidget::AdmAddClassWidget()
+{
+}
---
+++ Client/Plugins/Administrationv2/Implementation/ManageClasses/AdmAddClassWidget.h
@@ -0,0 +1,12 @@
+#ifndef ADMADDCLASSWIDGET_H
+#define ADMADDCLASSWIDGET_H
+
+#include <QWidget>
+
+class AdmAddClassWidget : public QWidget
+{
+public:
+    AdmAddClassWidget();
+};
+
+#endif // ADMADDCLASSWIDGET_H
