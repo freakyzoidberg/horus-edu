b8c6bf60298915a36172833ed55a1eda0c89463e
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-15 12:47:51 +0000

    [[Client]] [[Adm]] [[Manage classes]] * add field on widget

--- Client/Plugins/Administrationv2/Implementation/ManageClasses/AdmAddClassWidget.cpp
+++ Client/Plugins/Administrationv2/Implementation/ManageClasses/AdmAddClassWidget.cpp
@@ -1,5 +1,46 @@
+#include <QHBoxLayout>
+
 #include "AdmAddClassWidget.h"
 
 AdmAddClassWidget::AdmAddClassWidget()
 {
+    _mainLayout = new QVBoxLayout(this);
+    QHBoxLayout  *lineLayout;
+    QWidget      *line;
+
+    line = new QWidget();
+    lineLayout = new QHBoxLayout(line);
+    _mainLayout->addWidget(line);
+    this->_classNameLabel = new QLabel(tr("Nom de la classe:"));
+    this->_className = new QLineEdit();
+    lineLayout->addWidget(_classNameLabel);
+    lineLayout->addWidget(_className);
+
+    line = new QWidget();
+    _mainLayout->addWidget(line);
+    lineLayout = new QHBoxLayout(line);
+    this->_userReferentLabel = new QLabel(tr("Professeur principal:"));
+    this->_userReferent = new QComboBox();
+    initUserReferent();
+    this->_userReferent->setEditable(true);
+    lineLayout->addWidget(_userReferentLabel);
+    lineLayout->addWidget(_userReferent);
+
+    line = new QWidget();
+    _mainLayout->addWidget(line);
+    lineLayout = new QHBoxLayout(line);
+    this->_save = new QPushButton(tr("Enregister."));
+    this->_cancel = new QPushButton(tr("Abandonner."));
 }
+
+void    AdmAddClassWidget::initUserReferent()
+{
+
+}
+
+/*
+    id
+    nom
+    userReferent == prof principal / adm
+    salle referente (option)
+*/
