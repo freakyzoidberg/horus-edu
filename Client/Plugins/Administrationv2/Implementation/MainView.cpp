36e049bd4c1a0dbd7a255bcf5bcd23506d051ec5
Pierre <freakyzoidberg@users.noreply.github.com>
2009-11-15 17:24:41 +0000

    [[Admv2]] pluginsmanager

--- Client/Plugins/Administrationv2/Implementation/MainView.cpp
+++ Client/Plugins/Administrationv2/Implementation/MainView.cpp
@@ -6,6 +6,6 @@
 MainView::MainView(PluginManager *pluginManager)
 {
 	this->addTab(new AdmAddClassWidget(pluginManager->findPlugin<TreeDataPlugin *>(), pluginManager->findPlugin<UserDataPlugin *>()), tr("Classrooms"));
-    this->addTab(new ManageStudents(treeplugin, userplugin),tr("Students"));
+    this->addTab(new ManageStudents(pluginManager->findPlugin<TreeDataPlugin *>(), pluginManager->findPlugin<UserDataPlugin *>()),tr("Students"));
 	this->addTab(new ManageParents(this, pluginManager), tr("Parents"));
 }
