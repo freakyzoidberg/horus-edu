3c01c92ccc609b9996d9f0bc84ab189776b9ce9c
zoidberg <zoidberg@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-16 13:35:09 +0000

    [[admv2]] add edit en chantier mais ca fonctionne

--- trunk/Client/Plugins/Administrationv2/Implementation/ManageStudents/FormStudents.cpp
+++ trunk/Client/Plugins/Administrationv2/Implementation/ManageStudents/FormStudents.cpp
@@ -16,17 +16,27 @@ FormStudents::FormStudents()
 
     SuiInfos = new SuiviInfos();
     FormLayout->addWidget(SuiInfos);
-
+    id = 0;
     this->setLayout(FormLayout);
 }
 
 FormStudents::FormStudents(UserData *d)
 {
     QVBoxLayout *FormLayout = new QVBoxLayout(this);
+    id = d->id();
 
-    BasicInfos *BaseInfos = new BasicInfos();
-
+    BaseInfos = new BasicInfos(d);
     FormLayout->addWidget(BaseInfos);
+
+    SocInfos = new SocialInfos(d);
+    FormLayout->addWidget(SocInfos);
+
+
+    SchoInfos = new SchoolInfos(d);
+    FormLayout->addWidget(SchoInfos);
+
+    SuiInfos = new SuiviInfos(d);
+    FormLayout->addWidget(SuiInfos);
 
 
     this->setLayout(FormLayout);
