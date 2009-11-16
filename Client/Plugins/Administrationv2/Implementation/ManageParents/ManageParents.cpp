56b0aa8a31256844e10c284972a535749b80c63e
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-16 23:02:08 +0000

    [[Administration]] * reactive ManageParents

--- Client/Plugins/Administrationv2/Implementation/ManageParents/ManageParents.cpp
+++ Client/Plugins/Administrationv2/Implementation/ManageParents/ManageParents.cpp
@@ -3,14 +3,14 @@
 ManageParents::ManageParents(QWidget *parent, PluginManager *pluginManager) : QWidget(parent), _pluginManager(pluginManager)
 {
 	layout = new QBoxLayout(QBoxLayout::LeftToRight, this);
-	//list = new ListParents(this, pluginManager);
-	//layout->addWidget(list);
-	//connect(list, SIGNAL(editParent(UserData *)), this, SLOT(parentEdited(UserData *)));
+	list = new ListParents(this, pluginManager);
+	layout->addWidget(list);
+	connect(list, SIGNAL(editParent(UserData *)), this, SLOT(parentEdited(UserData *)));
 }
 
 void		ManageParents::parentEdited(UserData *user)
 {
-	//list->hide();
+	list->hide();
 	edit = new EditParent(this, _pluginManager, user);
 	layout->addWidget(edit);
 	connect(edit, SIGNAL(exit()), this, SLOT(editExited()));
@@ -20,5 +20,5 @@ void		ManageParents::editExited()
 {
 	disconnect(edit, SIGNAL(exit()), this, SLOT(editExited()));
 	delete edit;
-	//list->show();
+	list->show();
 }
