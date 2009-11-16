970f2328926ae77f41c41a8c6a8e7561b52edd84
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-16 21:50:47 +0000

    [[Administration]] * missing files

---
+++ Client/Plugins/Administrationv2/Implementation/ManageParents/ListParents.h
@@ -0,0 +1,39 @@
+#ifndef				__LISTPARENTS_H__
+# define			__LISTPARENTS_H__
+
+# include			<QWidget>
+
+# include			<QListView>
+# include			<QPushButton>
+# include			<QBoxLayout>
+
+# include			"../../../../../Common/PluginManager.h"
+# include			"../../../../../Common/UserData.h"
+# include			"DisplayParent.h"
+
+class				ListParents : public QWidget
+{
+	Q_OBJECT
+
+public:
+	ListParents(QWidget *parent, PluginManager *pluginManager);
+
+private:
+	QListView		*listView;
+	DisplayParent	*displayer;
+	QPushButton		*editButton;
+	QPushButton		*deleteButton;
+	QBoxLayout		*informationsLayout;
+	PluginManager	*_pluginManager;
+
+private slots:
+	void			parentSelected(const QModelIndex &current, const QModelIndex &previous);
+	void			parentAdded();
+	void			parentEdited();
+	void			parentDeleted();
+
+signals:
+	void			editParent(UserData *user);
+};
+
+#endif
