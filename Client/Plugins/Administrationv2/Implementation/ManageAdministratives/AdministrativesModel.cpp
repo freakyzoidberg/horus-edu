8a7126cb23c5ebaa42fb08e3881a39949d0edd31
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-19 14:52:15 +0000

    [[Administrationv2]] * delete Administrative model debugs

--- Client/Plugins/Administrationv2/Implementation/ManageAdministratives/AdministrativesModel.cpp
+++ Client/Plugins/Administrationv2/Implementation/ManageAdministratives/AdministrativesModel.cpp
@@ -1,7 +1,6 @@
 #include	"AdministrativesModel.h"
 
 #include	<QIcon>
-#include	<QDebug>
 
 AdministrativesModel::AdministrativesModel(const QHash<quint32, UserData*>&  _users, QObject *Administrative) : QAbstractListModel(Administrative), users(_users)
 {
@@ -10,12 +9,8 @@ AdministrativesModel::AdministrativesModel(const QHash<quint32, UserData*>&  _us
 QVariant	AdministrativesModel::data(const QModelIndex &index, int role) const
 {
 	int		i = 0;
-	qDebug() << "Asked: " << index.row() << " / " << index.column();
 	foreach (UserData* user, users)
 	{
-		qDebug() << user;
-		if (user->level() == LEVEL_ADMINISTRATOR)
-			qDebug() << "et un adm" << i;
 		if (user->level() != LEVEL_ADMINISTRATOR || user->status() == Data::DELETED || user->status() == Data::EMPTY)
 			continue;
 
@@ -31,7 +26,6 @@ QVariant	AdministrativesModel::data(const QModelIndex &index, int role) const
 		}
 		i++;
 	}
-	qDebug() << "not fouund";
 	return (QVariant());
 }
 
@@ -41,6 +35,5 @@ int			AdministrativesModel::rowCount(const QModelIndex &) const
 	foreach (UserData* user, users)
 		if (user->level() == LEVEL_ADMINISTRATOR && user->status() != Data::DELETED && user->status() != Data::EMPTY)
 			i++;
-	qDebug() << i;
 	return (i);
 }
