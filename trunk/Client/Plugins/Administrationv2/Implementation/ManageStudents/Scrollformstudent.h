a82d49dca619e06b0e9a3532575288ce6b83a323
zoidberg <zoidberg@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-19 19:10:22 +0000

    [[admv2]] essaye de corriger des segfault, sans visu car le plugin loarder est casse

--- trunk/Client/Plugins/Administrationv2/Implementation/ManageStudents/Scrollformstudent.h
+++ trunk/Client/Plugins/Administrationv2/Implementation/ManageStudents/Scrollformstudent.h
@@ -5,10 +5,13 @@
 class ScrollFormStudent : public QScrollArea
 {
 public:
-    ScrollFormStudent(QList<UserData*> list);
+    ScrollFormStudent(QList<UserData*> list, int idnode);
     FormStudents *StudentForm;
-    ScrollFormStudent(QList<UserData*> list, QList<UserData*> parentlist,UserData *d);
+    ScrollFormStudent(QList<UserData*> list, QList<UserData*> parentlist,UserData *d, int idnode);
     ~ScrollFormStudent();
+    int getnodeid() {return _nodeid;}
+private:
+    int _nodeid;
 };
 
 #endif // SCROLLFORMSTUDENT_H
