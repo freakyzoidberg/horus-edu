9b203f08ff2c2e3c243b41198d0df79aaaa3b770
git <git@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-25 11:58:21 +0000

    [[Data]] remove unused files

--- Client/Plugins/UserBaseClient/UserListModel.cpp
+++
@@ -1,7 +0,0 @@
-#include "UserListModel.h"
-#include "../../../Common/UserData.h"
-#include <QMimeData>
-#include <QDataStream>
-
-UserListModel::UserListModel(const UserDataPlugin* plugin) : DataListModel(plugin) {}
-
