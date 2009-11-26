3f5155fc8d99f538d275cd499a08553f9e512228
git <git@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-11-26 01:00:44 +0000

    Add LICENSE file, and add headers to every .cpp and .h

--- Client/Plugins/Administrationv2/Implementation/ManageClasses/AdmAddClassWidget.cpp
+++ Client/Plugins/Administrationv2/Implementation/ManageClasses/AdmAddClassWidget.cpp
@@ -1,3 +1,37 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Horus is free software: you can redistribute it and/or modify               *
+ * it under the terms of the GNU General Public License as published by        *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * Horus is distributed in the hope that it will be useful,                    *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with Horus. If not, see <http://www.gnu.org/licenses/>.               *
+ *                                                                             *
+ * The orginal content of this material was realized as part of                *
+ * 'Epitech Innovative Project' www.epitech.eu                                 *
+ *                                                                             *
+ * You are required to preserve the names of the original authors              *
+ * of this content in every copy of this material                              *
+ *                                                                             *
+ * Authors :                                                                   *
+ * - BERTHOLON Romain                                                          *
+ * - GRANDEMANGE Adrien                                                        *
+ * - LACAVE Pierre                                                             *
+ * - LEON-BONNET Valentin                                                      *
+ * - NANOUCHE Abderrahmane                                                     *
+ * - THORAVAL Gildas                                                           *
+ * - VIDAL Jeremy                                                              *
+ *                                                                             *
+ * You are also invited but not required to send a mail to the original        *
+ * authors of this content in case of modification of this material            *
+ *                                                                             *
+ * Contact: contact@horus-edu.net                                              *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include <QHBoxLayout>
 #include <QPushButton>
 #include <QDebug>
@@ -182,7 +216,8 @@ void    AdmAddClassWidget::addClassInDatabase()
 
 	save = user;
 	classSave = newClass;
-	connect(newClass, SIGNAL(created()), this, SLOT(modifUser()));
+	//connect(newClass, SIGNAL(created()), this, SLOT(modifUser()));
+	modifUser();
 }
 
 void    AdmAddClassWidget::modifUser()
@@ -192,7 +227,7 @@ void    AdmAddClassWidget::modifUser()
 	_classList->classList()->addItem(new QListWidgetItem(QIcon(":/Icons/desk.png"), classSave->name()));
 	_classList->classList()->addItem(QVariant(classSave->id()).toString());
 	_classList->classList()->setRowHidden(this->_classList->classList()->count() - 1, true);
-	disconnect(classSave, SIGNAL(created()), this, SLOT(modifUser()));
+//	disconnect(classSave, SIGNAL(created()), this, SLOT(modifUser()));
 }
 
 void    AdmAddClassWidget::choosenClass(QListWidgetItem *item)
--- Common/GradeDataPlugin.h
+++ Common/GradeDataPlugin.h
@@ -1,3 +1,37 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Horus is free software: you can redistribute it and/or modify               *
+ * it under the terms of the GNU General Public License as published by        *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * Horus is distributed in the hope that it will be useful,                    *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with Horus. If not, see <http://www.gnu.org/licenses/>.               *
+ *                                                                             *
+ * The orginal content of this material was realized as part of                *
+ * 'Epitech Innovative Project' www.epitech.eu                                 *
+ *                                                                             *
+ * You are required to preserve the names of the original authors              *
+ * of this content in every copy of this material                              *
+ *                                                                             *
+ * Authors :                                                                   *
+ * - BERTHOLON Romain                                                          *
+ * - GRANDEMANGE Adrien                                                        *
+ * - LACAVE Pierre                                                             *
+ * - LEON-BONNET Valentin                                                      *
+ * - NANOUCHE Abderrahmane                                                     *
+ * - THORAVAL Gildas                                                           *
+ * - VIDAL Jeremy                                                              *
+ *                                                                             *
+ * You are also invited but not required to send a mail to the original        *
+ * authors of this content in case of modification of this material            *
+ *                                                                             *
+ * Contact: contact@horus-edu.net                                              *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #ifndef GRADEDATAPLUGIN_H
 #define GRADEDATAPLUGIN_H
 
