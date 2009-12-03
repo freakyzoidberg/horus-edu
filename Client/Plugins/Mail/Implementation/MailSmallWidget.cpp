609bf7a7b066907b7069281428da789bc4670af2
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-12-03 17:38:01 +0000

    [[Mail]] * added missing files

---
+++ Client/Plugins/Mail/Implementation/MailSmallWidget.cpp
@@ -0,0 +1,25 @@
+#include					"MailSmallWidget.h"
+
+#include					<QListView>
+#include					<QBoxLayout>
+#include					<QSortFilterProxyModel>
+
+#include					"../../../../Common/Data.h"
+
+MailSmallWidget::MailSmallWidget(MailDataPlugin *mailPlugin) : _mailPlugin(mailPlugin)
+{
+	QBoxLayout				*layout;
+	QSortFilterProxyModel	*filter;
+	QListView				*view;
+
+	layout = new QBoxLayout(QBoxLayout::TopToBottom, this);
+	layout->setMargin(0);
+	layout->setSpacing(0);
+	view = new QListView(this);
+	layout->addWidget(view);
+	filter = new QSortFilterProxyModel(this);
+	filter->setSourceModel(mailPlugin->listModel());
+	filter->setFilterRole(Data::FILTER_ROLE);
+	filter->setFilterFixedString("INBOX");
+	view->setModel(filter);
+}
