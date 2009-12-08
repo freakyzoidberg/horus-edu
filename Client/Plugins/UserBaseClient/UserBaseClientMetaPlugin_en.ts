713159fc67cbbe0cc3f3e15637bc9fe3a4469582
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-12-08 14:34:09 +0000

    [[UserDataBase]] * filter headerData on role * updated translations

--- Client/Plugins/UserBaseClient/UserBaseClientMetaPlugin_en.ts
+++ Client/Plugins/UserBaseClient/UserBaseClientMetaPlugin_en.ts
@@ -1,22 +1,80 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="en_US">
+<context>
+    <name>DataListModel</name>
+    <message>
+        <location filename="../../DataListModel.cpp" line="68"/>
+        <source>Id</source>
+        <translation>Id</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="70"/>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="72"/>
+        <source>Last name</source>
+        <translation>Last name</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="74"/>
+        <source>First name</source>
+        <translation>First name</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="76"/>
+        <source>Birth place</source>
+        <translation>Birth place</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="78"/>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="80"/>
+        <source>Home phone</source>
+        <translation>Home phone</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="82"/>
+        <source>Mobile phone</source>
+        <translation>Mobile phone</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="84"/>
+        <source>Work phone</source>
+        <translation>Work phone</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="86"/>
+        <source>Social insurance number</source>
+        <translation>Social insurance number</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="88"/>
+        <source>Occupation</source>
+        <translation>Occupation</translation>
+    </message>
+</context>
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="../../../Common/Plugin.h" line="46"/>
+        <location filename="../../../Common/Plugin.h" line="75"/>
         <source>Plugin loaded : </source>
-        <translation type="unfinished"></translation>
+        <translation>Plugin loaded :</translation>
     </message>
 </context>
 <context>
     <name>UserDataBasePlugin</name>
     <message>
-        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="21"/>
-        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="22"/>
-        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="23"/>
+        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="53"/>
+        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="54"/>
+        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="55"/>
         <source>Nobody</source>
-        <translation type="unfinished"></translation>
+        <translation>Nobody</translation>
     </message>
 </context>
 </TS>
--- Client/Plugins/UserBaseClient/UserBaseClientMetaPlugin_fr.ts
+++ Client/Plugins/UserBaseClient/UserBaseClientMetaPlugin_fr.ts
@@ -1,22 +1,80 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>DataListModel</name>
+    <message>
+        <location filename="../../DataListModel.cpp" line="68"/>
+        <source>Id</source>
+        <translation>Id</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="70"/>
+        <source>Login</source>
+        <translation>Nom d&apos;utilisateur</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="72"/>
+        <source>Last name</source>
+        <translation>Nom de famille</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="74"/>
+        <source>First name</source>
+        <translation>Prénom</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="76"/>
+        <source>Birth place</source>
+        <translation>Lieu de naissance</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="78"/>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="80"/>
+        <source>Home phone</source>
+        <translation>Téléphone fixe</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="82"/>
+        <source>Mobile phone</source>
+        <translation>Téléphone portable</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="84"/>
+        <source>Work phone</source>
+        <translation>Téléphone bueau</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="86"/>
+        <source>Social insurance number</source>
+        <translation>Numéro de sécurité sociale</translation>
+    </message>
+    <message>
+        <location filename="../../DataListModel.cpp" line="88"/>
+        <source>Occupation</source>
+        <translation>Métier</translation>
+    </message>
+</context>
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="../../../Common/Plugin.h" line="46"/>
+        <location filename="../../../Common/Plugin.h" line="75"/>
         <source>Plugin loaded : </source>
-        <translation type="unfinished"></translation>
+        <translation>Plugin chargé :</translation>
     </message>
 </context>
 <context>
     <name>UserDataBasePlugin</name>
     <message>
-        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="21"/>
-        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="22"/>
-        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="23"/>
+        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="53"/>
+        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="54"/>
+        <location filename="../../../Common/DataImplementations/UserDataBase/UserDataBasePlugin.cpp" line="55"/>
         <source>Nobody</source>
-        <translation type="unfinished"></translation>
+        <translation>Personne</translation>
     </message>
 </context>
 </TS>
