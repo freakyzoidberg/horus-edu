302584495f89580fda31a0b009d296f8bd2aab65
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-12-11 16:46:57 +0000

    some display modifs

--- Client/Plugins/TimeTable/Implementation/EditSchedule.cpp
+++ Client/Plugins/TimeTable/Implementation/EditSchedule.cpp
@@ -40,43 +40,57 @@ EditSchedule::EditSchedule(ScheduleDataPlugin *sd, TreeDataPlugin *td, int id, i
     sdp = sd;
     tdp = td;
 	mainLayout = new QVBoxLayout(this);
-    mainLayout->setSpacing(0);
-    mainLayout->setMargin(0);
+   mainLayout->setSpacing(0);
+	mainLayout->setMargin(0);
+
     QLabel *title = new QLabel(tr("Dates de l\'emploi du temps"), this);
     title->setProperty("isFormTitle", true);
+	title->setMaximumHeight(30);
     mainLayout->addWidget(title);
 
+//classListView->setMaximumHeight(64);
     QFrame *eventFrame = new QFrame();
     eventFrame->setProperty("isFormFrame", true);
     QBoxLayout *eventMainLayout = new QBoxLayout(QBoxLayout::TopToBottom, eventFrame);
     eventMainLayout->setSpacing(0);
     eventMainLayout->setMargin(0);
-    datesLayout = new QGridLayout();
+
+	datesLayout = new QGridLayout();
     datesLayout->setSpacing(4);
     datesLayout->setMargin(8);
-   // datesLayout->setColumnMinimumWidth(0, 150);
     QLabel *startlabel = new QLabel(tr("Date de debut"), this);
+	startlabel->setMaximumHeight(30);
     startlabel->setProperty("isFormLabel", true);
     datesLayout->addWidget(startlabel, 0, 0);
     _startDate = new QDateEdit(this);
     _startDate->setCalendarPopup(true);
     _startDate->setDate(QDate::currentDate());
-    datesLayout->addWidget(_startDate, 0, 1);
+	datesLayout->addWidget(_startDate, 1, 0);
 
     QLabel *endlabel = new QLabel(tr("Date de fin"), this);
     endlabel->setProperty("isFormLabel", true);
-    datesLayout->addWidget(endlabel, 1, 0);
+	endlabel->setMaximumHeight(30);
+	datesLayout->addWidget(endlabel, 0, 1);
+
     _endDate = new QDateEdit(this);
     _endDate->setCalendarPopup(true);
     _endDate->setDate(QDate::currentDate());
-    datesLayout->addWidget(_endDate, 1, 1);
-    eventMainLayout->addLayout(datesLayout);
+
+	datesLayout->addWidget(_endDate, 1, 1);
+	eventMainLayout->addLayout(datesLayout);
     mainLayout->addWidget(eventFrame);
+
+	QLabel *t = new QLabel(tr("Ajout des vacances"), this);
+	t->setMaximumHeight(30);
+	t->setProperty("isFormTitle", true);
+	mainLayout->addWidget(t);
 
     if (type == 1)
         fillForm(id);
-    mainLayout->addWidget(new EditException());
-	mainLayout->addWidget(new QWidget());
+
+	EditException *edit = new EditException();
+	mainLayout->addWidget(edit);
+	mainLayout->addWidget(new QWidget()
 }
 
 void    EditSchedule::fillForm(int id)
