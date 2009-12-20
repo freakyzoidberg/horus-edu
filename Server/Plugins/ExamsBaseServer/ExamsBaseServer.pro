3c4649c871c3b60d598f46e105ce9b0adea4cea6
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2009-12-20 18:54:33 +0000

    [[Server]] * marks and exam data plugin

---
+++ Server/Plugins/ExamsBaseServer/ExamsBaseServer.pro
@@ -0,0 +1,25 @@
+# -------------------------------------------------
+# Project created by QtCreator 2009-04-09T19:51:57
+# -------------------------------------------------
+QT -= gui
+QT += sql
+DEFINES += HORUS_SERVER
+TEMPLATE = lib
+TARGET = ExamsBaseServerMetaPlugin
+SOURCES += \
+	../../../Common/DataImplementations/ExamsDataBase/ExamsDataBase.cpp \
+	../../../Common/DataImplementations/ExamsDataBase/ExamsDataBasePlugin.cpp
+HEADERS += \
+	../../../Common/DataImplementations/ExamsDataBase/ExamsDataBase.h \
+	../../../Common/DataImplementations/ExamsDataBase/ExamsDataBasePlugin.h \
+	../../../Common/ExamsData.h \
+	../../../Common/ExamsDataPlugin.h \
+	../../../Common/TreeData.h \
+	../../../Common/TreeDataPlugin.h \
+	../../../Common/MetaPlugin.h \
+	../../../Common/Plugin.h \
+	../../../Common/PluginManager.h \
+	../../../Common/Data.h \
+	../../../Common/DataPlugin.h \
+	../../../Common/DataManager.h \
+	ExamsBaseMetaPlugin.h
---
+++ Server/Plugins/MarksBaseServer/MarksBaseServerMetaPlugin.h
@@ -0,0 +1,57 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Horus is free software: you can redistribute it and/or modify               *
+ * it under the terms of the GNU General Public License as published by        *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * Horus is distributed in the hope that it will be useful,                    *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with Horus. If not, see <http://www.gnu.org/licenses/>.               *
+ *                                                                             *
+ * The orginal content of this material was realized as part of                *
+ * 'Epitech Innovative Project' www.epitech.eu                                 *
+ *                                                                             *
+ * You are required to preserve the names of the original authors              *
+ * of this content in every copy of this material                              *
+ *                                                                             *
+ * Authors :                                                                   *
+ * - BERTHOLON Romain                                                          *
+ * - GRANDEMANGE Adrien                                                        *
+ * - LACAVE Pierre                                                             *
+ * - LEON-BONNET Valentin                                                      *
+ * - NANOUCHE Abderrahmane                                                     *
+ * - THORAVAL Gildas                                                           *
+ * - VIDAL Jeremy                                                              *
+ *                                                                             *
+ * You are also invited but not required to send a mail to the original        *
+ * authors of this content in case of modification of this material            *
+ *                                                                             *
+ * Contact: contact@horus-edu.net                                              *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#ifndef MARKSBASESERVERMETAPLUGIN_H
+#define MARKSBASESERVERMETAPLUGIN_H
+
+#include "../../../Common/MetaPlugin.h"
+
+#include "../../../Common/DataImplementations/MarksDataBase/MarksDataBasePlugin.h"
+
+class MarksBaseServerMetaPlugin : public MetaPlugin
+{
+  Q_OBJECT
+  Q_INTERFACES(MetaPlugin)
+
+public:
+	inline MarksBaseServerMetaPlugin() {
+	  pluginList.append(new MarksDataBasePlugin);
+  }
+};
+
+/* maybe put this lines in a cpp if this file is include by an other file */
+// declare instance of the plugin
+Q_EXPORT_PLUGIN2(MarksBaseServerMetaPlugin, MarksBaseServerMetaPlugin);
+
+#endif // MARKSBASESERVERMETAPLUGIN_H
