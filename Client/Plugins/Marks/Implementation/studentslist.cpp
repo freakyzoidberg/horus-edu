95b2ab0dd00b72b2c99e5c7528c74dc2434fc520
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2010-01-02 19:57:05 +0000

    student list for exams

---
+++ Client/Plugins/Marks/Implementation/studentslist.cpp
@@ -0,0 +1,56 @@
+#include <QVBoxLayout>
+#include <QListWidget>
+#include <QLabel>
+#include <QList>
+
+#include "../../../../Common/UserDataPlugin.h"
+#include "../../../../Common/UserData.h"
+#include "studentslist.h"
+
+StudentsList::StudentsList(TreeData *node, PluginManager  *pluginManager)
+{
+	_pluginManager = pluginManager;
+	_node = node;
+
+	QVBoxLayout *ListLayout = new QVBoxLayout();
+
+	ListLayout->setMargin(0);
+	ListLayout->setSpacing(0);
+
+	_sList = new QListWidget(this);
+	_sList->setSelectionRectVisible(false);
+
+	QLabel *title = new QLabel(tr("Select a student to add, view or edit its grades."));
+	title->setProperty("isTitle", true);
+	ListLayout->addWidget(title);
+
+	ListLayout->addWidget(_sList);
+
+//	connect(_examsList, SIGNAL(itemClicked(QListWidgetItem *)),
+	//		this, SLOT(selectStudents(QListWidgetItem *)));
+	//connect(this->treePlugin,SIGNAL(dataUpdated(Data*)),this,SLOT(fillExamsList()));
+	this->setLayout(ListLayout);
+}
+
+void	StudentsList::fillStudentList()
+{
+	UserDataPlugin	*users = _pluginManager->findPlugin<UserDataPlugin *>();
+
+	QList<Data *> usersData = users->allDatas();
+	for (int i = 0; i < usersData.size(); ++i)
+	{
+		UserData *data = qobject_cast<UserData *>(usersData.at(i));
+		if (data->studentClass() == _node->parent())
+		{
+			QListWidgetItem *tempitem = new QListWidgetItem(QIcon(":/desk.png"),
+															data->name() + " " + data->surname());
+			tempitem->setData(Qt::UserRole, data->id());
+			_sList->addItem(tempitem);
+		}
+	}
+}
+
+void	StudentsList::studentSelection(QListWidgetItem *item)
+{
+
+}
