eab10df7c99e74bfe621aca00d507149c608fb8d
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2010-01-03 17:09:46 +0000

    another little modifs

--- Client/Plugins/Marks/Implementation/formaddgrade.h
+++ Client/Plugins/Marks/Implementation/formaddgrade.h
@@ -2,11 +2,28 @@
 #define FORMADDGRADE_H
 
 #include <QWidget>
+#include <QGridLayout>
+#include <QLineEdit>
+#include <QLabel>
+#include <QVariant>
+#include <QTextEdit>
 
 class FormAddGrade : public QWidget
 {
 public:
     FormAddGrade();
+	QGridLayout		*mainLayout()	{ return _mainLayout; }
+	QLabel			*commentLabel() { return _commentLabel; }
+	QTextEdit		*commentEdit()	{ return _commentEdit; }
+	QLabel			*markLabel()	{ return _markLabel; }
+	QLineEdit		*markEdit()		{ return _markEdit; }
+
+private:
+	QGridLayout		*_mainLayout;
+	QLabel			*_commentLabel;
+	QTextEdit		*_commentEdit;
+	QLabel			*_markLabel;
+	QLineEdit		*_markEdit;
 };
 
 #endif // FORMADDGRADE_H
