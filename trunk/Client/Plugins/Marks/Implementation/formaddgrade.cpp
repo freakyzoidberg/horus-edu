d4af943c8018d9934108f564436a2711bf749b94
romain <romain@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2010-01-03 17:09:46 +0000

    another little modifs

--- trunk/Client/Plugins/Marks/Implementation/formaddgrade.cpp
+++ trunk/Client/Plugins/Marks/Implementation/formaddgrade.cpp
@@ -1,26 +1,21 @@
-#include <QGridLayout>
-#include <QLineEdit>
-#include <QLabel>
-#include <QVariant>
-#include <QTextEdit>
 
 #include "formaddgrade.h"
 
 FormAddGrade::FormAddGrade()
 {
-	QGridLayout	*mainLayout = new QGridLayout(this);
-	QLabel		*commentLabel = new QLabel(tr("Commentaire:"));
-	QTextEdit	*commentEdit = new QTextEdit();
-	QLabel		*markLabel = new QLabel(tr("Note:"));
-	QLineEdit	*markEdit = new QLineEdit();
+	_mainLayout = new QGridLayout(this);
+	_commentLabel = new QLabel(tr("Commentaire:"));
+	_commentEdit = new QTextEdit();
+	_markLabel = new QLabel(tr("Note:"));
+	_markEdit = new QLineEdit();
 	QLabel *title = new QLabel(tr("Ajout d'une note."), this);
 	title->setProperty("isFormTitle", QVariant(true));
 	title->setMaximumHeight(30);
 
-	mainLayout->addWidget(title, 0, 0, 1, 2);
-	mainLayout->addWidget(commentLabel, 1, 0, 1, 2);
-	mainLayout->addWidget(commentEdit, 2, 0, 2, 2);
-	mainLayout->addWidget(markLabel,4, 0);
-	mainLayout->addWidget(markEdit, 4, 1);
-	mainLayout->addWidget(new QWidget(), 5, 0, 10, 2);
+	_mainLayout->addWidget(title, 0, 0, 1, 2);
+	_mainLayout->addWidget(_commentLabel, 1, 0, 1, 2);
+	_mainLayout->addWidget(_commentEdit, 2, 0, 2, 2);
+	_mainLayout->addWidget(_markLabel,4, 0);
+	_mainLayout->addWidget(_markEdit, 4, 1);
+	_mainLayout->addWidget(new QWidget(), 5, 0, 10, 2);
 }
