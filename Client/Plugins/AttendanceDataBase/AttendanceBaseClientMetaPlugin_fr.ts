24a375ec4af34d289ddd97111db9cfba0780148c
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2010-01-05 12:19:26 +0000

    [[Installer]] * removed calendar
    [[MainFrame]]
    * removed some useless smalldisplayables

---
+++ Client/Plugins/AttendanceDataBase/AttendanceBaseClientMetaPlugin_fr.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../../../Common/Plugin.h" line="75"/>
+        <source>Plugin loaded : </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
