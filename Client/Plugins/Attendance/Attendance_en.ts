5f474dedd4e4b948152c2454a8e93669fce13bb9
val <val@cb2ab776-01a4-11df-b136-7f7962f7bc17>
2010-01-06 21:38:36 +0000

    [[Translations]] * attendance

--- Client/Plugins/Attendance/Attendance_en.ts
+++ Client/Plugins/Attendance/Attendance_en.ts
@@ -1,72 +1,72 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="en_US">
 <context>
     <name>AddAttendance</name>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="112"/>
         <source>Informations:</source>
-        <translation type="unfinished"></translation>
+        <translation>Informations:</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="117"/>
         <source>Actions:</source>
-        <translation type="unfinished"></translation>
+        <translation>Actions:</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="121"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="123"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Apply</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="125"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Reset</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="127"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancel</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="168"/>
         <source>Absence informations</source>
-        <translation type="unfinished"></translation>
+        <translation>Attendance informations</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="176"/>
         <source>Last name</source>
-        <translation type="unfinished"></translation>
+        <translation>Last name</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="183"/>
         <source>First name</source>
-        <translation type="unfinished"></translation>
+        <translation>First name</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="190"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Date</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="198"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Type</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="202"/>
         <source>Absence</source>
-        <translation type="unfinished"></translation>
+        <translation>Absence</translation>
     </message>
     <message>
         <location filename="Implementation/AddAttendance.cpp" line="203"/>
         <source>Retard</source>
-        <translation type="unfinished"></translation>
+        <translation>Late</translation>
     </message>
 </context>
 <context>
@@ -74,12 +74,12 @@
     <message>
         <location filename="Implementation/Attendance.cpp" line="56"/>
         <source>Attendance</source>
-        <translation type="unfinished"></translation>
+        <translation>Attendance</translation>
     </message>
     <message>
         <location filename="Implementation/Attendance.cpp" line="78"/>
         <source>Attendances</source>
-        <translation type="unfinished"></translation>
+        <translation>Attendance</translation>
     </message>
 </context>
 <context>
@@ -87,37 +87,37 @@
     <message>
         <location filename="Implementation/CourPanel.cpp" line="50"/>
         <source>Cours informations</source>
-        <translation type="unfinished"></translation>
+        <translation>Class informations</translation>
     </message>
     <message>
         <location filename="Implementation/CourPanel.cpp" line="87"/>
         <location filename="Implementation/CourPanel.cpp" line="115"/>
         <source>Nom du Cour</source>
-        <translation type="unfinished"></translation>
+        <translation>Class name</translation>
     </message>
     <message>
         <location filename="Implementation/CourPanel.cpp" line="90"/>
         <location filename="Implementation/CourPanel.cpp" line="118"/>
         <source>Heure Debut</source>
-        <translation type="unfinished"></translation>
+        <translation>Start time</translation>
     </message>
     <message>
         <location filename="Implementation/CourPanel.cpp" line="93"/>
         <location filename="Implementation/CourPanel.cpp" line="121"/>
         <source>Heure Fin</source>
-        <translation type="unfinished"></translation>
+        <translation>End time</translation>
     </message>
     <message>
         <location filename="Implementation/CourPanel.cpp" line="96"/>
         <location filename="Implementation/CourPanel.cpp" line="124"/>
         <source>Appliquer</source>
-        <translation type="unfinished"></translation>
+        <translation>Apply</translation>
     </message>
     <message>
         <location filename="Implementation/CourPanel.cpp" line="164"/>
         <location filename="Implementation/CourPanel.cpp" line="166"/>
         <source>Absent</source>
-        <translation type="unfinished"></translation>
+        <translation>Absent</translation>
     </message>
 </context>
 <context>
@@ -125,52 +125,52 @@
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="55"/>
         <source>First name</source>
-        <translation type="unfinished"></translation>
+        <translation>First name</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="64"/>
         <source>Last name</source>
-        <translation type="unfinished"></translation>
+        <translation>Last name</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="71"/>
         <source>Unknow</source>
-        <translation type="unfinished"></translation>
+        <translation>Unknow</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="73"/>
         <source>Female</source>
-        <translation type="unfinished"></translation>
+        <translation>Female</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="75"/>
         <source>Male</source>
-        <translation type="unfinished"></translation>
+        <translation>Male</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="78"/>
         <source>Gender</source>
-        <translation type="unfinished"></translation>
+        <translation>Gender</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="87"/>
         <source>Email</source>
-        <translation type="unfinished"></translation>
+        <translation>Email</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="96"/>
         <source>Home phone</source>
-        <translation type="unfinished"></translation>
+        <translation>Home phone</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="105"/>
         <source>Work phone</source>
-        <translation type="unfinished"></translation>
+        <translation>Work phone</translation>
     </message>
     <message>
         <location filename="Implementation/InformationsUser.cpp" line="114"/>
         <source>Mobile phone</source>
-        <translation type="unfinished"></translation>
+        <translation>Mobile phone</translation>
     </message>
 </context>
 <context>
@@ -178,47 +178,47 @@
     <message>
         <location filename="Implementation/ListUser.cpp" line="54"/>
         <source>Select an user to add or show his attendance.</source>
-        <translation type="unfinished"></translation>
+        <translation>Select an user to add or show his attendance.</translation>
     </message>
     <message>
         <location filename="Implementation/ListUser.cpp" line="56"/>
         <source>Add attendance</source>
-        <translation type="unfinished"></translation>
+        <translation>Add an absent</translation>
     </message>
     <message>
         <location filename="Implementation/ListUser.cpp" line="57"/>
         <source>Show attendances</source>
-        <translation type="unfinished"></translation>
+        <translation>Show attendance</translation>
     </message>
     <message>
         <location filename="Implementation/ListUser.cpp" line="58"/>
         <source>Clean attendances</source>
-        <translation type="unfinished"></translation>
+        <translation>Clean attendance</translation>
     </message>
     <message>
         <location filename="Implementation/ListUser.cpp" line="133"/>
         <source>Informations:</source>
-        <translation type="unfinished"></translation>
+        <translation>Informations:</translation>
     </message>
     <message>
         <location filename="Implementation/ListUser.cpp" line="140"/>
         <source>Actions:</source>
-        <translation type="unfinished"></translation>
+        <translation>Actions:</translation>
     </message>
     <message>
         <location filename="Implementation/ListUser.cpp" line="240"/>
         <source>Confirmation</source>
-        <translation type="unfinished"></translation>
+        <translation>Confirmation</translation>
     </message>
     <message>
         <location filename="Implementation/ListUser.cpp" line="240"/>
         <source>Do you really want to delete </source>
-        <translation type="unfinished"></translation>
+        <translation>Do you really want to delete </translation>
     </message>
     <message>
         <location filename="Implementation/ListUser.cpp" line="240"/>
         <source> ?</source>
-        <translation type="unfinished"></translation>
+        <translation>?</translation>
     </message>
 </context>
 <context>
@@ -226,7 +226,7 @@
     <message>
         <location filename="../../../Common/Plugin.h" line="75"/>
         <source>Plugin loaded : </source>
-        <translation type="unfinished"></translation>
+        <translation>Plugin loaded : </translation>
     </message>
 </context>
 <context>
@@ -234,68 +234,68 @@
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="86"/>
         <source>Informations:</source>
-        <translation type="unfinished"></translation>
+        <translation>Informations:</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="91"/>
         <source>Actions:</source>
-        <translation type="unfinished"></translation>
+        <translation>Actions:</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="101"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Back</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="121"/>
         <source>Absences de </source>
-        <translation type="unfinished"></translation>
+        <translation>Attendance of </translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="136"/>
         <source>Nom du Cour</source>
-        <translation type="unfinished"></translation>
+        <translation>Class name</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="139"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Date</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="142"/>
         <source>Heure Debut</source>
-        <translation type="unfinished"></translation>
+        <translation>Start time</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="145"/>
         <source>Heure Fin</source>
-        <translation type="unfinished"></translation>
+        <translation>End time</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="148"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Type</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="151"/>
         <location filename="Implementation/ShowAttendance.cpp" line="187"/>
         <source>Effacer</source>
-        <translation type="unfinished"></translation>
+        <translation>Delete</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="178"/>
         <source>Absence</source>
-        <translation type="unfinished"></translation>
+        <translation>Absence</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="180"/>
         <source>Retard</source>
-        <translation type="unfinished"></translation>
+        <translation>Late</translation>
     </message>
     <message>
         <location filename="Implementation/ShowAttendance.cpp" line="193"/>
         <source>L&apos;utilisateur choisi n&apos;a pas d&apos;absences</source>
-        <translation type="unfinished"></translation>
+        <translation>Selected user has no attendance</translation>
     </message>
 </context>
 </TS>
