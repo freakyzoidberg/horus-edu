d476f11d0ff58422ddf55ec5d8b871b43793e108
Pierre <freakyzoidberg@users.noreply.github.com>
2010-01-06 21:45:33 +0000

    [[Trans]] en/fr Mails Schedule Timetable

--- Client/Plugins/TimeTable/Timetable_fr.ts
+++ Client/Plugins/TimeTable/Timetable_fr.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="Implementation/AdmClassListSelection.cpp" line="54"/>
         <source>Select a class to view, add or edit its time table.</source>
-        <translation type="unfinished">Sélectionnez une classe pour afficher, ajouter ou modifier son calendrier.</translation>
+        <translation>Sélectionnez une classe pour afficher, ajouter ou modifier son calendrier.</translation>
     </message>
 </context>
 <context>
@@ -15,43 +15,43 @@
         <location filename="Implementation/EdtScene.cpp" line="57"/>
         <location filename="Implementation/EdtScene.cpp" line="114"/>
         <source>monday</source>
-        <translation type="unfinished">lundi</translation>
+        <translation>lundi</translation>
     </message>
     <message>
         <location filename="Implementation/EdtScene.cpp" line="58"/>
         <location filename="Implementation/EdtScene.cpp" line="115"/>
         <source>tuesday</source>
-        <translation type="unfinished">mardi</translation>
+        <translation>mardi</translation>
     </message>
     <message>
         <location filename="Implementation/EdtScene.cpp" line="59"/>
         <location filename="Implementation/EdtScene.cpp" line="116"/>
         <source>wednesday</source>
-        <translation type="unfinished">mercredi</translation>
+        <translation>mercredi</translation>
     </message>
     <message>
         <location filename="Implementation/EdtScene.cpp" line="60"/>
         <location filename="Implementation/EdtScene.cpp" line="117"/>
         <source>thursday</source>
-        <translation type="unfinished">jeudi</translation>
+        <translation>jeudi</translation>
     </message>
     <message>
         <location filename="Implementation/EdtScene.cpp" line="61"/>
         <location filename="Implementation/EdtScene.cpp" line="118"/>
         <source>friday</source>
-        <translation type="unfinished">vendredi</translation>
+        <translation>vendredi</translation>
     </message>
     <message>
         <location filename="Implementation/EdtScene.cpp" line="62"/>
         <location filename="Implementation/EdtScene.cpp" line="119"/>
         <source>saturday</source>
-        <translation type="unfinished">samedi</translation>
+        <translation>samedi</translation>
     </message>
     <message>
         <location filename="Implementation/EdtScene.cpp" line="63"/>
         <location filename="Implementation/EdtScene.cpp" line="120"/>
         <source>sunday</source>
-        <translation type="unfinished">dimanche</translation>
+        <translation>dimanche</translation>
     </message>
 </context>
 <context>
@@ -59,22 +59,22 @@
     <message>
         <location filename="Implementation/EditSchedule.cpp" line="48"/>
         <source>Il n&apos;y a pas d&apos;options pour cette action, vous n&apos;avez qu&apos;a valider</source>
-        <translation type="unfinished">il n&apos;y a pas d&apos;options pour cette action, vous n&apos;avez qu&apos;a valider.</translation>
+        <translation>il n&apos;y a pas d&apos;options pour cette action, vous n&apos;avez qu&apos;a valider</translation>
     </message>
     <message>
         <location filename="Implementation/EditSchedule.cpp" line="65"/>
         <source>Date de debut</source>
-        <translation type="unfinished">date de debut</translation>
+        <translation>date de debut</translation>
     </message>
     <message>
         <location filename="Implementation/EditSchedule.cpp" line="75"/>
         <source>Date de fin</source>
-        <translation type="unfinished">date de fin</translation>
+        <translation>date de fin</translation>
     </message>
     <message>
         <location filename="Implementation/EditSchedule.cpp" line="87"/>
         <source>Ajout des vacances</source>
-        <translation type="unfinished">Ajout de vacances</translation>
+        <translation>Ajout de vacances</translation>
     </message>
 </context>
 <context>
@@ -82,122 +82,122 @@
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="79"/>
         <source>Informations du cour</source>
-        <translation type="unfinished">Informations du cour</translation>
+        <translation>Informations du cour</translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="86"/>
         <source>Nom :</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="102"/>
         <source>Heure de debut : </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="109"/>
         <source>Heure de fin :</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="117"/>
         <source>Professeur :</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="123"/>
         <source>Jour :</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="127"/>
         <source>Lundi</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="128"/>
         <source>Mardi</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="129"/>
         <source>Mercredi</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="130"/>
         <source>Jeudi</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="131"/>
         <source>Vendredi</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="132"/>
         <source>Samedi</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="133"/>
         <source>Dimanche</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="135"/>
         <source>Details</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="140"/>
         <source>Couleurs</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="144"/>
         <source>White</source>
-        <translation type="unfinished">Blanc</translation>
+        <translation>Blanc</translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="145"/>
         <source>Yellow</source>
-        <translation type="unfinished">Jaune</translation>
+        <translation>Jaune</translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="146"/>
         <source>Green</source>
-        <translation type="unfinished">Vert</translation>
+        <translation>Vert</translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="147"/>
         <source>Blue</source>
-        <translation type="unfinished">Bleu</translation>
+        <translation>Bleu</translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="148"/>
         <source>Red</source>
-        <translation type="unfinished">Rouge</translation>
+        <translation>Rouge</translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="157"/>
         <source>Une semaine sur deux :</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="167"/>
         <source>Appliquer le cour meme en cas de vacances</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="170"/>
         <source>Date de debut</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EditScheduleEvent.cpp" line="178"/>
         <source>Date de fin</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -205,57 +205,57 @@
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="85"/>
         <source>Informations:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="98"/>
         <source>Actions:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="104"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="105"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="106"/>
         <source>Reset</source>
-        <translation type="unfinished">Reinitialliser</translation>
+        <translation>Reinitialliser</translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="107"/>
         <source>Cancel</source>
-        <translation type="unfinished">Annuler</translation>
+        <translation>Annuler</translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="108"/>
         <source>Add</source>
-        <translation type="unfinished">Ajouter</translation>
+        <translation>Ajouter</translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="109"/>
         <source>Edit</source>
-        <translation type="unfinished">Editer</translation>
+        <translation>Editer</translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="110"/>
         <source>Delete</source>
-        <translation type="unfinished">Supprimer</translation>
+        <translation>Supprimer</translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="301"/>
         <source>L&apos;heure de debut doit etre inferieur a l&apos;heure de fin</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/EdtWidget.cpp" line="308"/>
         <source>La date de debut doit etre inferieur a la date de fin</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -263,17 +263,17 @@
     <message>
         <location filename="Implementation/MainView.cpp" line="46"/>
         <source>Weekly view</source>
-        <translation type="unfinished">Vue semaine</translation>
+        <translation>Vue semaine</translation>
     </message>
     <message>
         <location filename="Implementation/MainView.cpp" line="47"/>
         <source>View planning</source>
-        <translation type="unfinished">Vue planning</translation>
+        <translation>Vue planning</translation>
     </message>
     <message>
         <location filename="Implementation/MainView.cpp" line="51"/>
         <source>Manage EDT</source>
-        <translation type="unfinished">Gestion des EDT</translation>
+        <translation>Gestion des EDT</translation>
     </message>
 </context>
 <context>
@@ -281,47 +281,47 @@
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="59"/>
         <source>Informations:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="70"/>
         <source>Actions:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="74"/>
         <source>Delete this edt</source>
-        <translation type="unfinished">Supprimer</translation>
+        <translation>Supprimer</translation>
     </message>
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="75"/>
         <source>Edit this edt</source>
-        <translation type="unfinished">Editer</translation>
+        <translation>Editer</translation>
     </message>
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="76"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="77"/>
         <source>Apply</source>
-        <translation type="unfinished">Appliquer</translation>
+        <translation>Appliquer</translation>
     </message>
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="78"/>
         <source>Reset</source>
-        <translation type="unfinished">Reinitialiser</translation>
+        <translation>Reinitialiser</translation>
     </message>
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="79"/>
         <source>Cancel</source>
-        <translation type="unfinished">Annuler</translation>
+        <translation>Annuler</translation>
     </message>
     <message>
         <location filename="Implementation/ManageEdt.cpp" line="80"/>
         <source>Add</source>
-        <translation type="unfinished">Ajouter</translation>
+        <translation>Ajouter</translation>
     </message>
 </context>
 <context>
@@ -329,7 +329,7 @@
     <message>
         <location filename="../../../Common/Plugin.h" line="75"/>
         <source>Plugin loaded : </source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -337,7 +337,7 @@
     <message>
         <location filename="Implementation/TimeTable.cpp" line="70"/>
         <source>TimeTable</source>
-        <translation type="unfinished">Emploi du Temps</translation>
+        <translation>Emploi du Temps</translation>
     </message>
 </context>
 </TS>
